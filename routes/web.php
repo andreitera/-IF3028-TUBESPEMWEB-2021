95fc283e7934b12564d48d2e268d7131d3be7a32
Null29 <muhammad.118140136@student.itera.ac.id>
2020-12-03 22:55:36 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('edit', "LaporanController@edit");
+Route::post('update', "LaporanController@update");
+Route::post('create', "LaporanController@create");
+Route::post('index', "LaporanController@index");
+Route::post('show', "LaporanController@show");
+Route::post('delete', "LaporanController@destroy");
