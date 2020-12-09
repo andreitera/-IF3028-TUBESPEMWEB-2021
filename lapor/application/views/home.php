55e209dc0de4f752d2f2a92c0b3a6667e2dbbd94
leo viranda <50198556+leoviranda@users.noreply.github.com>
2020-12-09 15:11:27 +0700

    view home dan css

---
+++ lapor/application/views/home.php
@@ -0,0 +1,18 @@
+	<div class="search">
+		<form action="" method="post">
+			<input type="text">
+			<button type="submit">Cari</button>
+		</form>
+	</div>
+	<div class="buat-lapor">
+		<a href="<?= base_url('buatlapor')?>">
+			Buat Laporan/Komentar
+			<img src="<?= base_url()?>/assets/img/Plus.svg" alt="">
+		</a>
+	</div>
+	<div class="container-laporan">
+		<p>Laporan/Komentar Terakhir</p>
+		<div class="line"></div>
+	</div>
+</body>
+</html>
