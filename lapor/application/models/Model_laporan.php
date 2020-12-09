d4916269eb043d9a270874c30313a6fd048b62bd
leo viranda <50198556+leoviranda@users.noreply.github.com>
2020-12-09 15:10:19 +0700

    view header
    view ini akan diload ketika halaman lainnya diload

---
+++ lapor/application/models/Model_laporan.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_laporan extends CI_Model {
+
+}
---
+++ lapor/application/views/header.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Lapor</title>
+    <link rel="stylesheet" href="<?= base_url()?>/assets/css/style.css" type="text/css">
+</head>
+<body>
+	<div class="header">
+		<h1>LAPOR!</h1>
+	</div>
