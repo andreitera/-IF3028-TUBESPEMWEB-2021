9f1636003e501d5619da4e5aece880ec49eedfeb
yustika-apz <yustika.zal@gmail.com>
2020-12-13 18:00:40 +0700

    Halaman login dan register

--- TubesPemWeb/routes/web.php
+++ TubesPemWeb/routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
 });
