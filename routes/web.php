31eed1eeaf03a5bd7ca1a8a77ac3238b45d778dc
ikhbaaalll <ikhbal.madrid17@gmail.com>
2020-12-14 07:03:58 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',                                     'HomeController@index')->name('home');
+Route::get('lapor',                                 'LaporController@index')->name('lapor');
+Route::get('buatlapor',                             'LaporController@create')->name('laporbuatView');
+Route::post('buatlapor',                            'LaporController@store')->name('laporbuat');
+Route::get('lapor/show/{id}',                       'LaporController@show')->name('laporShow');
+Route::get('success',                               'LaporController@sendEmail')->name('sendemail');
+Route::get('lapor/{unique_id}/edit',                'LaporController@edit')->name('laporedit');
+Route::post('lapor/{unique_id}/edit',               'LaporController@update')->name('laporupdate');
+Route::post('lapor/{unique_id}/delete',             'LaporController@destroy')->name('laporhapus');
