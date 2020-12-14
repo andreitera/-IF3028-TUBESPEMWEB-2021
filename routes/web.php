4611f1d1c1054f3fd99b5fb08583f0cb8b3b8fdd
ikhbaaalll <ikhbal.madrid17@gmail.com>
2020-12-14 23:36:37 +0700

    add post getuniqid to route

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',                                     'HomeController@index')->name('home');
+Route::get('/',                                     'LaporController@home')->name('home');
 Route::get('lapor',                                 'LaporController@index')->name('lapor');
-Route::get('buatlapor',                             'LaporController@create')->name('laporbuatView');
-Route::post('buatlapor',                            'LaporController@store')->name('laporbuat');
+Route::get('lapor/create',                          'LaporController@create')->name('laporbuatView');
+Route::post('lapor/create',                         'LaporController@store')->name('laporbuat');
 Route::get('lapor/show/{id}',                       'LaporController@show')->name('laporShow');
 Route::get('success',                               'LaporController@sendEmail')->name('sendemail');
 Route::get('lapor/{unique_id}/edit',                'LaporController@edit')->name('laporedit');
 Route::post('lapor/{unique_id}/edit',               'LaporController@update')->name('laporupdate');
 Route::post('lapor/{unique_id}/delete',             'LaporController@destroy')->name('laporhapus');
+Route::post('lapor/getuniqid',                      'LaporController@getUniqueId')->name('laporcek');
