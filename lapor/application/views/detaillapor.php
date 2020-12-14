a1dc62a8b1490af453e20de02bc5ae6272912f08
leo viranda <50198556+leoviranda@users.noreply.github.com>
2020-12-14 23:59:09 +0700

    membuat view detail laporan dan controller detail laporan

---
+++ lapor/application/views/detaillapor.php
@@ -0,0 +1,30 @@
+<div class="container-laporan">
+		<p>Detail Laporan/Komentar</p>
+		<div class="line"></div>
+
+        <div class="data">
+            <!-- ini untuk isi laporan -->
+        </div>
+
+        <div class="data">
+            <label for="">Lampiran</label><br>
+            <!-- ini untuk lampiran -->
+        </div>
+
+        <div class="data" style="float: left;">
+            <label for="">Waktu: </label>
+            <!-- ini untuk timestamp -->
+        </div>
+
+        <div class="data" style="float: left;"> 
+            <label for="">Aspek: </label>
+        </div>
+
+        <div class="data" style="float: right;"> 
+            <button class="submit" type="submit">Hapus Laporan/Komentar</button>        
+        </div>
+
+        <div class="line" style="clear: both;"></div>
+	</div>
+</body>
+</html>
