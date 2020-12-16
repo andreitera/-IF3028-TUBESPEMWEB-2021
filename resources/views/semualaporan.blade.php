bf5e4544cfda438c0f6d944f6e79a94890a9fef1
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-17 00:38:49 +0700

    Semua Laporan

---
+++ resources/views/semualaporan.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Utama</title>
+</head>
+<body>
+    <ul>
+        @foreach ($data as $report)
+            <li>
+                {{ $report->laporan }}
+                <a href="{{ route('show', $report->id) }}">Lihat Selengkapnya</a>
+            </li>
+        @endforeach
+    </ul>
+</body>
+</html>
