45141e0dc09eeb62b0cd0de95225b646ca83a099
RinggoGalihSadewo <ringgo.118140076@student.itera.ac.id>
2020-12-16 19:19:20 +0700

    default tampilan_view

---
+++ Tubes/application/views/tampilan_view.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tampilan Laporan</title>
+
+
+
+	<link rel="stylesheet" type="text/css" href="style/style_tampilan.css">
+
+</head>
+<body>
+
+	<div class="container">
+		
+		<div class="content">
+
+		<div class="header">
+	
+			<strong>Layanan Aspirasi dan Pengaduan Online Mahasiswa IF ITERA</strong>
+			
+			<br><br>
+
+		</div>
+
+		<form action="" method="POST">
+			<center>
+			<input type="text" name="cari">
+			<button type="submit" name="btncari">Cari</button>
+			</center>
+		</form>
+
+		<br>
+		<center>
+		<a href="#">Buat Laporan/Komentar</a>
+		</center>
+		
+
+		<br>
+
+		<hr>
+			<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+
+			<p>Lampiran File: Nama File </p>
+
+			<div class="detail">Tampilan Waktu <a href="#">Lihat Selengkapnya</a></div>
+			
+
+		<hr>
+			<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			
+			<p>Lampiran File: Nama File </p>
+
+			<div class="detail">Tampilan Waktu <a href="#">Lihat Selengkapnya</a></div>
+
+		<hr>
+			<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+
+			<p>Lampiran File: Nama File </p>
+
+			<div class="detail">Tampilan Waktu <a href="#">Lihat Selengkapnya</a></div>
+
+		</div>
+
+		<br><br>
+
+	</div>
+
+</body>
+</html>
