7f12f07c961b981e41784dd108a4d726142164ba
Muhammad Najie <57042601+muhammadnajie@users.noreply.github.com>
2020-12-16 14:14:36 +0700

    add page detail + css

---
+++ app/Views/detail.php
@@ -0,0 +1,16 @@
+<main>
+    <div class="container">
+        <h3 class="pt-2">Detail Laporan/Komentar</h3>
+        <article id="detail">
+            <p class="pt-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non blanditiis suscipit doloribus voluptate officia provident eos facere vitae, laboriosam, neque consectetur nulla obcaecati similique perferendis architecto dolor ullam. Voluptate, natus?</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem deleniti voluptate, sapiente, provident accusantium eligendi numquam hic blanditiis placeat tempore pariatur atque eveniet totam ullam? Dolores voluptatum aperiam distinctio numquam?</p>
+            <p class="pt-2">Lampiran:</p>
+            <img src="bg_bggenerator_com.png" width="100" height="100" alt="">
+            <div class="d-flex mt-2">
+                <p class="flex-grow-1">Waktu: 16-12-2020 10:00</p>
+                <p class="flex-grow-1">Aspek : Mahasiswa</p>
+                <p class="text-right flex-grow-2">Hapus Laporan/Komentar</p>
+            </div>
+        </article>
+    </div>
+</main>
