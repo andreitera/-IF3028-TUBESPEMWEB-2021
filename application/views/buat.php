f0fcfa7cd810ef2428aba77e0ed31cac000f9dca
nardiyansah <janatri1398@gmail.com>
2020-12-16 08:48:12 +0700

    buat tampilan sederhana halaman buat laporan (buat.php)

---
+++ application/views/buat.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buat Laporan</title>
+</head>
+<body>
+    <h1>SIMPLE LAPOR!</h1>
+    <div>Buat Laporan/Komentar</div>
+    <hr>
+    <form action="">
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <select name="" id="">
+            <option value="" disabled selected>Pilih Aspek Pelaporan/Komentar</option>
+            <option value="1">Agama</option>
+        </select>
+        <input type="file" accept="image/*,audio/*,video/*,.doc,.docx,.xls,.xlsx,.ppt,.pptx,.pdf,.txt">
+        <button type="submit">Buat LAPOR!</button>
+    </form>
+</body>
+</html>
