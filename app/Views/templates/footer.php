12c93e14b9e3be842961ec3b138ed5add6c76d41
Muhammad Najie <57042601+muhammadnajie@users.noreply.github.com>
2020-12-16 14:06:43 +0700

    add page utama, template header dan footer

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,12 @@
+<footer class="text-center">
+        <div class="container">
+            <p>Made with &#9825;</p>
+            <p>
+                by <a href="https://github.com/muhammadnajie">Najie</a> / 
+                <a href="https://github.com/muhkhoirurrizqi">Khoirurrizqi</a> /
+                <a href="https://github.com/khoirul-roziq">Rojik</a>
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
