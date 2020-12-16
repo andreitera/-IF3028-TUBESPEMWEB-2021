bd9f0a8ec7b25af12725c79f5b2738fa89f38aee
TioFazri <tiofajri@yahoo.co.id>
2020-12-17 01:42:18 +0700

    init + config + building view & css

---
+++ application/views/v_home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="asset/css/home.css">
+    <title>Lapor!</title>
+</head>
+<body>
+        <h1>SIMPLE LAPOR!</h1>
+    <br>
+        <form action="" class="form_div">
+            <input type="text" class="search_bar">
+            <button class="search_btn">CARI</button>
+        </form>
+        <a href="<?php base_url();?>buat">Buat Laporan/Komentar</a>
+    <br>
+        <p>Laporan/Komentar Terakhir</p>
+    <hr>
+    <br>
+        <img src="asset/icon/menu.png" class="more_pic" alt="TITIK3">
+    <br>
+        <img src="asset/icon/menu.png" class="more_pic" alt="TITIK3">
+</body>
+
+</html>
---
+++ application/views/v_tambah.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="asset/css/tambah.css">
+    <title>Document</title>
+</head>
+<body>
+        <h1>SIMPLE LAPOR!</h1>
+        <p>Buat Laporan/Komentar</p>
+    <hr>
+        <form action="">
+            <textarea name="" id="" placeholder="Laporan/Komentar"></textarea>
+            <br>
+            <select name="" id="">
+                <option value="">Pilih Pelapor</option>
+                <option value="">Mahasiswa</option>
+                <option value="">Dosen</option>
+                <option value="">Staff</option>
+                <option value="">Infrastruktur</option>
+            </select>
+        <br>
+            <input type="file">
+        <br>
+            <button class="buat_btn">Buat LAPOR!</button>
+    </form>
+    <br>
+    <br>
+    <hr>
+</body>
+</html>
