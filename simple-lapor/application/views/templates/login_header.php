6b4a72a2fac608fc7c61e99103506271357fe5f6
dederodhatul <dederodhatulfarida@gmail.com>
2020-12-16 12:36:28 +0700

    menambahkan templates header masing-masing halaman

---
+++ simple-lapor/application/views/templates/login_header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= base_url('assets/css/login_style.css') ?>">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <title><?= $title; ?></title>
+</head>
+
+<body>
