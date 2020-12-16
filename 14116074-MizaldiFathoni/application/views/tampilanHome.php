b4dfedd3a464fd0a68e73d4bd9553926309d431f
mizaldifathoni <55985698+mizaldifathoni@users.noreply.github.com>
2020-12-16 09:42:58 +0700

    menambahkan css

--- 14116074-MizaldiFathoni/application/views/tampilanHome.php
+++ 14116074-MizaldiFathoni/application/views/tampilanHome.php
@@ -6,18 +6,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
   <title>Sistem Lapor!</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/tampilanHome.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 </head>
 <body>
     
-    <h1 align="center">SIMPLE LAPOR!</h1>
+    <h1>SIMPLE LAPOR!</h1>
 
-    <form align="center" action="#" method="POST">
+    <form action="#" method="POST">
         <input type="text" name="cari">
         <button type="submit" name="submit"><i class="fa fa-search"> CARI</i></button>
       </form>
-      <p align="center">Buat Laporan/Komentar
-        <i class="fa fa-plus-square"></i></p><br>
+      <p align="center">
+      <a href="#">Buat Laporan/Komentar
+        <i class="fa fa-plus-square"></i></p></a>
+
       <label>Laporan/Komentar Terakhir</label>
   <hr>
       
