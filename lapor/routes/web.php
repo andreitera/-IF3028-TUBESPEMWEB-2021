b5d4d9cd1d619e69319d9dc36acbdb1da161b7d6
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2020-12-18 00:56:47 +0700

    Add profile page

--- lapor/routes/web.php
+++ lapor/routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'LaporController@listView')->name('listView');
+Route::get('/profil', 'ProfileController@showProfile')->name('showProfile');
