7426d3faffd160f5122a89c129e48a5f0fa54de3
vinaoktariana <vinaoktariana3@gmai.com>
2020-12-17 19:25:44 +0700

    update lihatlaporan

--- resources/views/lihatlaporan.blade.php
+++ resources/views/lihatlaporan.blade.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lihat Laporan</title>
 </head>
 <body>
-    <h1>SIMPLE LAPOR!</h1>
-    <p>Detail Laporan/Komentar</p>
-    -------------------------------
+<div class="header">
+            <header style="text-align: center;margin: 50px;">
+                <h1>IF SIAGA LAPOR!</h1>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP1NcsHf_auML9glkHz5cIuBjXgz4m_0Mw7Q&usqp=CAU.jpg"alt="Informatika" width="400px" height="150px"> 
+            </header>
+        </div>
+    	<div class="head">
+            Detail Laporan/Komentar
+		</div>
+		<hr>
+
     <p>{{$report->laporan}}</p>
     <p>{{$report->lampiran}}</p>
     <p>{{$data->created_at}}</p>
@@ -23,5 +29,6 @@
         @csrf
         <button type="submit">Hapus Laporan/Komentar</button>
     </form>
+    <hr> 
 </body>
 </html>
