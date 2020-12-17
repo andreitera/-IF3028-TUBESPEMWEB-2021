d22dd26c1eeaef418258044da22bb908e6d37d20
Muhammad Ichsan Al Fatih <muhammad.14116126@student.itera.ac.id>
2020-12-17 21:42:25 +0700

    Menghapus index dan menambah view
    Menghapus index dengan cara mengganti .htaccess pada CI dan menambahkan halaman detail laporan serta CSSnya

--- application/views/v_hasil.php
+++ application/views/v_hasil.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="asset/css/hasil.css">
     <title>Detail Laporan</title>
 </head>
 <body>
@@ -10,6 +11,15 @@
 <h1>SIMPLE LAPOR!</h1>
 <p>Detail Laporan/Komentar</p>
 <hr>
+<p>Lampiran</p>
+<br>
+<br>
+<p>Waktu</p>
+<p>Aspek</p>
+
+<a class='ubah'>Ubah Laporan/Komentar </a>
+<br>
+<hr>	
 
     
 </body>
