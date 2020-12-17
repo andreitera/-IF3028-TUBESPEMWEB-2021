8b085d4a34ddf173384e19dbb33dfe5d79b26580
febbysartika <febby.118140137@student.itera.ac.id>
2020-12-17 09:28:49 +0700

    Menambahkan fungsi ubah pada models

---
+++ application/models/Lapor_model.php
@@ -0,0 +1,38 @@
+<?php
+
+class Lapor_model extends CI_model
+{
+	
+
+	public function ubah($id){
+
+		//menset waktu saat ini
+		date_default_timezone_set('Asia/Jakarta');
+		$tanggal = date("Y-m-d H:i:s");
+
+		//mengolah file
+		$file = $_FILES['File'];
+		if($file = ''){}else{
+			$config['upload_path'] = './asset/file';
+			$config['allowed_types'] = 'jpeg|jpg|png|gif|pdf';
+
+			$this->load->library('upload',$config);
+			if(!$this->upload->do_upload('File')){
+				echo "gagal";
+			}else{
+				$file = $this->upload->data('file_name');
+			}
+		}
+
+			$isi = $this->input->post('isi');
+			$aspek = $this->input->post('aspek');
+
+		$this->db->set('isi', $isi);
+		$this->db->set('aspek', $aspek);
+		$this->db->set('tanggal', $tanggal);
+		$this->db->set('file', $file);
+		$this->db->where('id', $id);
+		$this->db->update('lapor');
+	}
+
+}
