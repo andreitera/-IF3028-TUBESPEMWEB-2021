7b3bdabd0bb6e1fae9e32d6dc09f2fc914fc09cb
desradha <ddesradha@gmail.com>
2020-12-18 00:46:08 +0700

    js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+	// menu bar
+	document.getElementById("lapor-menu").addEventListener("click", function () {
+		document.getElementById("lapor-menu").classList.toggle("change");
+		document.getElementById("nav-show").classList.toggle("change");
+	});
+	// menu bar
+}
