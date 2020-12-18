8d5f4091b85ed483e577c9e8454eaeeee34bd838
Muhammad Ichsan Al Fatih <muhammad.14116126@student.itera.ac.id>
2020-12-19 01:06:13 +0700

    Update Validasi, JS, CSS
    Mengupdate halaman validasi dan JSnya beserta style CSSnya

--- application/views/v_hasil.php
+++ application/views/v_hasil.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="asset/css/hasil.css">
+    <script src="assets/js/validasi.js"></script>
     <title>Detail Laporan</title>
 </head>
 <body>
@@ -11,13 +12,36 @@
 <h1>SIMPLE LAPOR!</h1>
 <p>Detail Laporan/Komentar</p>
 <hr>
-<p>Lampiran</p>
+<br>
+<div class="arsip">
+    <?php foreach ($detail as $hm )  :?>
+        <?php $id = $hm['id']; ?>
+        <p><?= $hm['isi']; ?> </p>
+    <?php endforeach; ?>
+ </div>
+ <br>
+<p class="arsip">Lampiran</p>
 <br>
 <br>
-<p>Waktu</p>
-<p>Aspek</p>
+<div class="waktu">
+    <?php 
+    $date = new DateTime($hm['waktu']);
+    echo 'Waktu : ';
+    echo $date->format('d-m-Y H:i');
+    ?>
+</div> 
+<div class="aspek">
+    <?php
+    echo 'Aspek : '; 
+    echo  $hm['aspek']; 
+    ?>
+</div>
 
-<a class='ubah'>Ubah Laporan/Komentar </a>
+<?php
+    echo "
+    <a class='hapus' href= javascript:confirmDelete('hapus?hapus_id=$id')>Hapus Laporan/Komentar  > </a>           
+    <a class='waktu2' href='ubah?ubah_id=$id'>Ubah Laporan/Komentar  > </a>";
+?>
 <br>
 <hr>	
 
--- application/views/v_tambah.php
+++ application/views/v_tambah.php
@@ -1,16 +1,34 @@
-<!DOCTYPE html>
+<script>
+function tambahdata(){
+    var laporan=$("[name='laporan']").val();
+    var aspek=$("[name='aspek']").val();
+    var file=$("[name='myFile']").val();
+
+    $.ajax({
+        type:'POST',
+        data:'laporan='+laporan+'&aspek='+aspek+'&myFile='+file,
+        url:'<?php echo base_url().'buat/tambahdata' ?>',
+        dataType :'json',
+        success: function(hasil){
+            console.log(hasil);
+        }
+    });
+}
+</script>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="asset/css/tambah.css">
-    <title>Document</title>
+    <script src="assets/js/validasi.js"></script>
+    <title><?php echo $judul; ?></title>
 </head>
 <body>
         <h1>SIMPLE LAPOR!</h1>
         <p>Buat Laporan/Komentar</p>
     <hr>
-        <form action="<?php echo base_url(). 'crud/tambah_aksi';?>" method="POST">
+        <form action="<?php echo base_url(). 'crud/tambah_aksi';?>" onSubmit="return validasi(this)" method="POST">
             <textarea name="isi" id="" placeholder="Laporan/Komentar"></textarea>
             <br>
             <select name="aspek" id="">
@@ -21,9 +39,9 @@
                 <option value="Infra">Infrastruktur</option>
             </select>
         <br>
-            <input type="file">
+            <input id="myFile" type="file" name="myFile">
         <br>
-            <button class="buat_btn">Buat LAPOR!</button>
+        <input class="submit submit1" name="uploadFile" type="submit" value="Buat LAPOR!"><br>
     </form>
     <br>
     <br>
