16e6468b2db77894db958598ceb57184affcb93e
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-18 20:49:37 +0700

    add link css

--- resources/views/editlaporan.blade.php
+++ resources/views/editlaporan.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buat Laporan/Komentar</title>
+    <link rel="stylesheet" href=" {{ asset('css/styleSearch.css') }} ">
 </head>
 <body>
     <h1>SIMPLE LAPOR!</h1>
@@ -23,5 +24,6 @@
             <button type="submit" style="display: block; padding-top: 5px;">Ganti LAPOR!</button>
             --------------------------------------
     </form>
+    
 </body>
 </html>
