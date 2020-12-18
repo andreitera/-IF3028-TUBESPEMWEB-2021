5b92cd9f0d06e3b9429b678de313c0efb9527e8f
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-18 20:53:23 +0700

    add menu

--- resources/views/semualaporan.blade.php
+++ resources/views/semualaporan.blade.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Utama</title>
+    <title>Utama </title>
+    <link rel="stylesheet" href=" {{ asset('css/style.css') }} ">
 </head>
 <body>
+
+<p>Tampilkan Laporan</p>
+
+<form action="{{ route('semualaporan', $data->id) }}" method="POST">
+        @csrf
+        <button type="submit">Tampilkan Semua Laporan</button>
+    </form>
     <ul>
         @foreach ($data as $report)
             <li>
