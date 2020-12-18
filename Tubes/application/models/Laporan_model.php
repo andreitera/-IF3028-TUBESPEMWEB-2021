5724dcb3659d6e4dcfb4b3a11adb0ceba7021923
andika-118140058 <andikas926@gmail.com>
2020-12-18 15:24:45 +0700

    Tambah Laporan_controller

--- Tubes/application/models/Laporan_model.php
+++ Tubes/application/models/Laporan_model.php
@@ -1,4 +1,5 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Laporan_model extends CI_Model{
     private $_table = "laporan";
