bf7f9bc8ad250e326ab52a088c96dccaf1530178
Juni-Simamora16 <65211893+Juni-Simamora16@users.noreply.github.com>
2020-12-19 01:53:39 +0700

    Membuat input form pada model

---
+++ application/models/input_form.php
@@ -0,0 +1,36 @@
+<?php
+class input_form extends CI_Model{
+    public function __construct(){
+        $this->load->database();
+    }   
+    
+    public function do_upload(){
+        $config['upload_path'] = '../images/';
+        $config['allowed_types'] = 'jpg|png|jpeg';
+        $config['max_size']  = '2048';
+        $config['remove_space'] = TRUE;
+      
+        $this->load->library('upload', $config); 
+
+        if($this->upload->do_upload('lampiran')){ 
+          $return = array('result' => 'success', 'file' => $this->upload->data(), 'error' => '');
+          return $return;
+        }else{
+          $return = array('result' => 'failed', 'file' => '', 'error' => $this->upload->display_errors());
+          return $return;
+        }
+      }
+    
+    public function create($upload) 
+    {
+        $data = array(
+            'judul' => $this->input->post('judul'),
+            'kolom_komentar' => $this->input->post('komen'),
+            'lampiran' => $upload['file']['file_name'],
+            'aspek_pelaporan' => $this->input->post('pilihan')
+          );
+
+          return $this->db->insert('laporan', $data);
+    }
+}
+?>
