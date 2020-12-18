fd07420bea3e1fb910c0a4e044b85321ae37247d
nardiyansah <janatri1398@gmail.com>
2020-12-18 20:54:02 +0700

    mengubah besar textarea pada views/buat.php

--- application/views/buat.php
+++ application/views/buat.php
@@ -6,11 +6,11 @@
     <title>Buat Laporan</title>
 </head>
 <body>
-    <h1>SIMPLE LAPOR!</h1>
+    <h1 style="text-align: center">SIMPLE LAPOR!</h1>
     <div>Buat Laporan/Komentar</div>
     <hr>
     <form action="<?= base_url('buat/tambah'); ?>" method="POST" enctype="multipart/form-data">
-        <textarea name="isi" id="input_isi" cols="30" rows="10"></textarea>
+        <textarea name="isi" id="input_isi" cols="160" rows="10"></textarea>
         <select name="id_aspek" id="input_aspek">
             <option value="" disabled selected>Pilih Aspek Pelaporan/Komentar</option>
             <?php foreach($aspek as $row){ ?>
