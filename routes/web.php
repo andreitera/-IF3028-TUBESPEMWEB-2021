bb4f998a150296b159a5d8d503fd93ec4728158c
ribkajsidabutar <ribkajsidabutar@gmail.com>
2020-12-18 18:33:15 +0700

    menambahkan route di web

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',                 'LaporanController@home')->name('home');
+
+Route::get('/daftarlaporan',            'LaporanController@index')->name('daftarlaporan');
+Route::get('/buatlaporan',            'LaporanController@create')->name('buatlaporan');
+Route::post('/buatlaporan',            'LaporanController@store')->name('lapor');
+Route::post('/editlaporan/{id}',            'LaporanController@edit')->name('edit');
+Route::post('/editlaporan/{id}/edit',            'LaporanController@update')->name('editlaporan');
+Route::post('/hapuslaporan/{id}',            'LaporanController@destroy')->name('hapus');
+Route::get('/laporan/{id}',             'LaporanController@show')->name('lihatlaporan');
+Route::post('/carilaporan',             'LaporanController@cari')->name('cari');
