01479a99b95c4fb5c7538880e9bd1ed1ac104923
vinaoktariana <vinaoktariana3@gmai.com>
2020-12-19 02:39:08 +0700

    edit laporan

--- resources/views/editlaporan.blade.php
+++ resources/views/editlaporan.blade.php
@@ -9,21 +9,32 @@
 <body>
     <h1>SIMPLE LAPOR!</h1>
     <p>Buat Laporan/Komentar</p>
-    ---------------------------------
+    <hr>
     <form action="{{ route('storeedit', $data->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <input type="textarea" name="laporan" value="{{$data->laporan}}" autocomplete="off" style="display: block; padding-top: 5px;">
+        <div>
+            Pengirim:
+            <input type="text" name="pengirim" value="{{$data->pengirim}}">
+        </div>
+        <div>
+            Judul:
+            <input type="text" name="judul" value="{{$data->judul}}">
+        </div>
+        <div class="main">
+            <textarea rows="10" cols="100" name="laporan" value="{{$data->laporan}}"placeholder="Laporan/Komentar" required></textarea>
+        </div><br>
         <select name="aspek" value="{{$data->aspek}}" style="display: block; padding-top: 5px;">
                 <option value="1">Pilih Aspek Pelaporan/Komentar</option>
                 <option value="Dosen">Dosen</option>
                 <option value="Mahasiswa">Mahasiswa</option>
                 <option value="Infrastruktur">Infrastruktur</option>
                 <option value="Pengajaran">Pengajaran</option> 
-            </select>
-            <!-- <input type="file" name="lampiran" required style="display: block; padding-top: 5px;"> -->
+        </select><br>
+        <div class="file">
+            <input type="file" name="lampiran" value="{{$data->lampiran}}" required style="display: block; padding-top: 5px">
+        </div><br>
             <button type="submit" style="display: block; padding-top: 5px;">Ganti LAPOR!</button>
-            --------------------------------------
-    </form>
+        <br>
     
 </body>
 </html>
