7ce9e4d80f9928d26a0fdf9a4c6dae60113cd67a
vinaoktariana <vinaoktariana3@gmai.com>
2020-12-19 00:42:53 +0700

    formlapor (fix)

--- resources/views/formlapor.blade.php
+++ resources/views/formlapor.blade.php
@@ -1,50 +1,61 @@
 <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Buat Laporan/Komentar</title>
-            <link rel="stylesheet" href="{{ asset('css/style.css') }}"type="text/css">
-        </head>
+<html>
+
+<head>
+    <title>Buat Laporan/Komentar</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+</head>
+
 <body>
-@if (session('msg'))
-        {{ session('msg') }}
+    @if (session('msg'))
+    {{ session('msg') }}
     @endif
 
-<div class="container">  
-    <div class="content">
-		<div class="header">
-            <header style="text-align: center;margin: 50px;">
-                <h1>IF SIAGA LAPOR!</h1>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP1NcsHf_auML9glkHz5cIuBjXgz4m_0Mw7Q&usqp=CAU.jpg"alt="Informatika" width="400px" height="150px"> 
-            </header>
+    <div class="container">
+        <div class="content">
+            <div class="header">
+                <header style="text-align: center;margin: 50px;">
+                    <h1>IF SIAGA LAPOR!</h1>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP1NcsHf_auML9glkHz5cIuBjXgz4m_0Mw7Q&usqp=CAU.jpg" alt="Informatika" width="400px" height="150px">
+                </header>
+            </div>
+            <div class="head">
+                Buat Laporan/Komentar
+            </div>
+            <hr>
+            <form method="post" action="{{ route('store')}}" enctype="multipart/form-data">
+                @csrf
+                <div>
+                    Pengirim:
+                    <input type="text" name="pengirim">
+                </div>
+                <div>
+                    Judul:
+                    <input type="text" name="judul">
+                </div>
+                <div class="main">
+                    <textarea rows="10" cols="100" name="laporan" placeholder="Laporan/Komentar" required></textarea>
+                </div><br>
+                <div class="aspek">
+                    <select name="aspek" required>
+                        <option value="1">Pilih Aspek Pelaporan/Komentar</option>
+                        <option value="Dosen">Dosen</option>
+                        <option value="Staff">Staff</option>
+                        <option value="Mahasiswa">Mahasiswa</option>
+                        <option value="Infrastruktur">Infrastruktur</option>
+                        <option value="Pengajaran">Pengajaran</option>
+                    </select>
+                </div><br>
+                <div class="file">
+                    <input type="file" name="lampiran" required style="display: block; padding-top: 5px;">
+                </div><br>
+                <div class="btn_submit">
+                    <button type="submit" name="buat" style="display: block; padding-top: 5px;">Buat LAPOR!</button>
+                </div>
+            </form><br>
+            <hr>
         </div>
-    	<div class="head">
-        	Buat Laporan/Komentar
-		</div>
-		<hr>
-	<form method="post" action="{{ route('store')}}" enctype="multipart/form-data">
-    @csrf
-    <div class="main">
-		<textarea rows="10" cols="100" name="laporan" placeholder="Laporan/Komentar" required></textarea>
-	</div><br>
-    <div class="aspek">
-        <select name="aspek" required>
-        <option value="1">Pilih Aspek Pelaporan/Komentar</option> 
-                <option value="Dosen">Dosen</option>
-                <option value="Staff">Staff</option>
-                <option value="Mahasiswa">Mahasiswa</option>
-                <option value="Infrastruktur">Infrastruktur</option>
-                <option value="Pengajaran">Pengajaran</option> 		
-        </select>
-	</div><br>
-   <div class="file">
-        <input type="file" name="lampiran" required style="display: block; padding-top: 5px;">
-	</div><br>
-    <div class="btn_submit">
-        <button type="submit" name="buat" style="display: block; padding-top: 5px;">Buat LAPOR!</button>
     </div>
-	</form><br>
-    <hr>
-    </div>
-</div>
 </body>
+
 </html>
