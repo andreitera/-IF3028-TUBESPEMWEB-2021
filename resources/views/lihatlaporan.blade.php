68457eb34655ce2b7141b8ed8c47e1629042bd55
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-18 20:51:50 +0700

    add link css

--- resources/views/lihatlaporan.blade.php
+++ resources/views/lihatlaporan.blade.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lihat Laporan</title>
+    <link rel="stylesheet" href=" {{ asset('css/style.css') }} ">
 </head>
 <body>
     <h1>SIMPLE LAPOR!</h1>
