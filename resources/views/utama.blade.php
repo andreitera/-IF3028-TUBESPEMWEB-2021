63fe18024de27a56170bfc02ce15fc18d2bc4530
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-18 20:53:51 +0700

    add menu

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -4,24 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Utama</title>
+    <link rel="stylesheet" href=" {{ asset('css/style.css') }} ">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href=" {{ asset('css/styleUtama.css') }} ">
 </head>
 <body>
-    <h1>SIMPLE LAPOR!</h1>
-    <form action="" method="POST">
-    <label for="">
-        <input type="text" name="cari" autocomplete="off" >
-    </label>
-    <button name="cari">Cari</button>
-    </form>
-    <a href="{{ url('/formlapor') }}">Buat laporan/komentar</a>
 
-    <ul>
-        @foreach ($reports as $report)
-            <li>
-                {{ $report->laporan }}
-                <a href="/reports/{{ $report->id }}">Lihat Selengkapnya</a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <div class="title">
+            <h1>Simple Lapor!</h1>
+        </div>
+        
+        <form action="" method="POST">
+            <label for="">
+                <input type="text" name="cari" autocomplete="off" >
+            </label>
+            <button name="cari">Cari</button>
+        </form>
+        <a href="{{ url('/formlapor') }}">Buat laporan/komentar</a>
+    
+        <ul>
+            @foreach ($data as $report)
+                <li>
+                    {{ $report->laporan }}
+                    <a href="/reports/{{ $report->id }}">Lihat Selengkapnya</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+
+    <script> 
+        console.log("hello");
+    </script>
 </body>
 </html>
