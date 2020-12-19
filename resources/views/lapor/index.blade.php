337524123d0b5a310636d1cfc39c3d367524135d
ryznocaHub <fikrihalim27@gmail.com>
2020-12-19 10:05:15 +0700

    Form buat LAPOR!

--- resources/views/lapor/index.blade.php
+++ resources/views/lapor/index.blade.php
@@ -1,12 +1,39 @@
 @extends('master.master')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
 @endsection
 
 @section('isi')
-<div class="home">
-    <img src="{{ asset('image/home-cover.jpg') }}" alt="home-cover" class="home-cover">
-    <img src="{{ asset('image/bawahheader.svg') }}" alt="home-white" class="home-white">
+<div class="judul">
+    <div class="judul-kata1">Layanan Aspirasi dan Pengaduan Online Teknik Informatika ITERA</div>
+    <div class="judul-kata2">Sampaikan Laporan Anda Langsung Kepada Pihak Terkait</div>
+    <div class="judul-bar"></div>
+</div>
+<div class="form-buat">
+        <form method="POST" action="{{ route('laporbuat') }}" enctype="multipart/form-data" class="form">
+        Buat Laporan/Komentar
+        {{-- <div class="form-container">
+            <div class="form-kategori">Prodi</div>
+            <div class="form-kategori">Dosen</div>
+            <div class="form-kategori">Mata Kuliah</div>
+        </div> --}}
+        <div class="bar"></div>
+        <textarea id="laporan" name="laporan" placeholder="Laporan/Komentar"></textarea>
+        <select name="aspek" id="aspek">
+            <option value="" disabled selected>Pilih Aspek Pelaporan/Komentar</option>
+            <option value="Dosen">Dosen</option>
+            <option value="Mata Kuliah">Mata Kuliah</option>
+            <option value="Prodi">Prodi</option>
+        </select>
+        <br>
+        <input type="button" value="Choose File" onclick="document.getElementById('pic').click()" class="btn">
+        <input type="text" id="filename" value="No File Choosen">
+        <input type="file" id="pic" name="pic" style="display:none" onchange="document.getElementById('filename').value=this.value">
+        <div class="container">
+            <button id="submit" class="btn">Buat LAPOR!</button>
+        </div>
+        <div class="bar"></div>
+    </form>
 </div>
 @endsection
