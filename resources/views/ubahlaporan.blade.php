605e41ef63947bd31e48a47ee475767283f43924
risnoputri07 <risnoputri2@gmail.com>
2020-12-19 20:39:14 +0700

    mengubah tampilan untuk edit laporan

--- resources/views/ubahlaporan.blade.php
+++ resources/views/ubahlaporan.blade.php
@@ -8,41 +8,115 @@
     *{
         box-sizing: border-box;
     }
+    
     body{
-    background: #F08080;
+        background: #F08080;
+    }
+    
+    .container{
+        max-width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 30px;
+        padding-right: 30px;
+        position: relative;
+    }
+    
+    .header{
+        text-align : center;
+        font-size : 24px;
+    }
+
+    .laporarea textarea{
+        width : 100%;
+        height : 200px;
+        display : inline-block;
+    }
+    
+    .optionarea{
+        margin-top   :  20px;
+        margin-bottom:  20px;
+    }
+    .submitarea{
+        text-align : right ;
+        margin-right : 370px;
+    }
+
+    .submitarea{
+        /* margin-right: 80%;
+        margin-left : 20%; */
+        margin : 15px;
+        display : inline-block;
+        float: right;
+    }
+    .filearea{
+        display: inline;
+        float: left;
     }
    
     </style>
 </head>
 <body>
+<div class="container">
+    <div class="header">
+        <h1>SIMPLE LAPOR!!</h1>
+    </div>
+    
+    <div class="conten">
     <form action="{{ route('editlaporan', $edit->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <label for="">
-        Pengirim:
-        <input type="text" name="pengirim" value="{{ $edit->pengirim }}" required>
-    </label> <br>
-    <label for="">
-        Judul:
-        <input type="text" name="judul" value="{{ $edit->judul }}" required>
-    </label><br>
-    <label for="">
-        Laporan:
+        <div class="pengirimarea">
+            <label for="">
+            <p>Pengirim  :</p>
+            <input type="text" name="pengirim" value="{{ $edit->pengirim }}" required>
+            </label>    
+            <br>        
+        </div>
+
+        <div class="judularea">
+               <label for="">
+             <p>Judul     :</p>
+            <input type="text" name="judul" value="{{ $edit->judul }}" required>
+              </label>
+            <br>
+         </div>
+
+        <div class="laporarea">
+             <label for="">
+        <p>Laporan   :</p>
         <textarea name="isilaporan" rows="4" column="50" required></textarea>
-    </label><br>
-    <label for="">
-        Aspek:
-        <select name="aspect" id="">
-            <option value="Mahasiswa">Mahasiswa</option>
-            <option value="Dosen">Dosen</option>
-            <option value="Pengajaran">Pengajaran</option>
-            <option value="Infrastruktur">Infrastruktur</option>
-        </select>
-    </label><br>
-    <label for="">
-        File:
-        <input type="file" name="file">
-    </label>
-    <button type="submit">Edit Lapor</button>
-    </form>
+            </label>
+             <br>
+
+        </div>
+
+
+        <div class="optionarea">
+            <label for="">
+                Aspek     :
+            <select name="aspect" id="">
+                <option value="Mahasiswa">Mahasiswa</option>
+                <option value="Dosen">Dosen</option>
+                <option value="Pengajaran">Pengajaran</option>
+                <option value="Infrastruktur">Infrastruktur</option>
+            </select>
+            </label>
+            <br>
+        </div>
+
+        <div class="footer">
+            <div class="filearea">
+                <label for="">
+                    File      :
+                <input type="file" name="file">
+                </label>
+            </div>
+
+            <div class="submitarea">    
+                <button type="submit">Edit Lapor</button>    
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
