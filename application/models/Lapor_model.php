0aadf7473c1971a8ff84862133f8c39106f254eb
SultanRaffy <sraffy15@gmail.com>
2020-12-19 14:18:31 +0700

    update file models

--- application/models/Lapor_model.php
+++ application/models/Lapor_model.php
@@ -1,39 +1,27 @@
 <?php
-
-class Lapor_model extends CI_model
+class Lapor_model extends CI_Model
 {
-	// public function ubah($id){
+    public function __construct()
+    {
+        $this->load->database();
+    }
 
-	// 	//menset waktu saat ini
-	// 	date_default_timezone_set('Asia/Jakarta');
-	// 	$tanggal = date("Y-m-d H:i:s");
-
-	// 	//mengolah file
-	// 	$file = $_FILES['File'];
-	// 	if($file = ''){}else{
-	// 		$config['upload_path'] = './asset/file';
-	// 		$config['allowed_types'] = 'jpeg|jpg|png|gif|pdf';
-
-	// 		$this->load->library('upload',$config);
-	// 		if(!$this->upload->do_upload('File')){
-	// 			echo "gagal";
-	// 		}else{
-	// 			$file = $this->upload->data('file_name');
-	// 		}
-	// 	}
-
-	// 		$isi = $this->input->post('isi');
-	// 		$aspek = $this->input->post('aspek');
-
-	// 	$this->db->set('isi', $isi);
-	// 	$this->db->set('aspek', $aspek);
-	// 	$this->db->set('tanggal', $tanggal);
-	// 	$this->db->set('file', $file);
-	// 	$this->db->where('id', $id);
-	// 	$this->db->update('lapor');
-	// }
-	public function uploadLaporan($table, $data)
-	{
-		return $this->db->insert($table, $data);
-	}
+    public function tampil_data($id = FALSE)
+    {
+        if ($id == FALSE) {
+            $query = $this->db->get('lapor');
+            return $query->result_array();
+        }
+        $query = $this->db->get_where('lapor', array('id_laporan' => $id));
+        return $query->row_array();
+    }
+    public function hapus_data($where, $table)
+    {
+        $this->db->where($where);
+        $this->db->delete($table);
+    }
+    public function uploadLaporan($table, $data)
+    {
+        return $this->db->insert($table, $data);
+    }
 }
