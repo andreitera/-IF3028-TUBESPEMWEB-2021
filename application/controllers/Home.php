2c95d7bdbb1aac2a8decbb5056ac771cf0b8f593
alvionita108 <60423164+alvionita108@users.noreply.github.com>
2020-12-19 17:11:20 +0700

    update controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,24 +2,25 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Home_model');
+		$this->load->helper('form');
+		$this->load->helper('url');
+	}
 
-    function __construct(){
-        parent::__construct();
-            $this->load->model('Model_home', 'm');
-            $this->load->helper('form');
-            $this->load->helper('url');
+	public function index()
+	{
+		$data ['judul'] = "Tugas Besar PWB 118140048-118140103-118140105"; 
+		$data ['home'] = $this->Home_model->getAllHome();
+		$this->load->view('home/index',$data);
+	
+	}
 
-    }
+	// public function ambildata()	{
+    //     $dataLaporan = $this->Home_model->ambildatanya->result();
+    //     echo json_encode($dataLaporan);
+    // }
 
-	public function index()	{
-        $data['title'] = "Tugas Besar PWB 118140048-118140103-118140105"; 
-		$this->load->view('home/index', $data);
-    }
-    
-    public function ambildata()	{
-        $dataLaporan = $this->m->ambildata('laporkuy')->result();
-        echo json_encode($dataLaporan);
-    }
 }
-
-?>
