1e50dc053dc9256be12f4e9052d5a252eec78b7f
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-19 14:15:04 +0700

    update semua laporan fix

--- resources/views/semualaporan.blade.php
+++ resources/views/semualaporan.blade.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Utama </title>
     <link rel="stylesheet" href=" {{ asset('css/style.css') }} ">
 </head>
+
 <body>
 
-<p>Tampilkan Laporan</p>
+    <p>Tampilkan Laporan</p>
 
-<form action="{{ route('semualaporan', $data->id) }}" method="POST">
-        @csrf
-        <button type="submit">Tampilkan Semua Laporan</button>
-    </form>
     <ul>
-        @foreach ($data as $report)
-            <li>
-                {{ $report->laporan }}
-                <a href="{{ route('show', $report->id) }}">Lihat Selengkapnya</a>
-            </li>
+        @foreach ($data as $data)
+        <li>
+            {{ $data->laporan }}
+            <a href="{{ route('show', $data->id) }}">Lihat Selengkapnya</a>
+        </li>
         @endforeach
     </ul>
 </body>
+
 </html>
