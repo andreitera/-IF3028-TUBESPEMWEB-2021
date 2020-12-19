cbdefb0c118881d161d10b765b59350d593f8d3a
Leonard Rizta Anugerah Perdana <leonardrizta3@gmail.com>
2020-12-19 17:23:36 +0700

    add search-bar

---
+++ lapor/resources/views/Lapor/layout/search.blade.php
@@ -0,0 +1,8 @@
+<section id="search-bar">
+    <div class="container ">
+		<form action="/search">
+			<input type="search" class="" name="q" placeholder="Kata kunci atau tracking ID..." autocomplete="off" value="">
+			{{-- <img src="{{ asset('asset/images/icons/search.svg') }}" alt="" class="search-icon"> --}}
+		</form>
+	</div>
+</section>
