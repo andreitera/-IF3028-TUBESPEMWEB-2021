a6249b6e84199bf0ebb5f356dbaa1b7d21ceb70e
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-19 23:36:12 +0700

    update pencarian

--- resources/views/pencarian.blade.php
+++ resources/views/pencarian.blade.php
@@ -1,12 +1,22 @@
 @extends('Master.master')
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/styleCari.css') }}">
+
+@endsection
 
 @section('content')
-<h1>Berikut hasil pencarian</h1>
 
+<div class="jumbotron">
+<h1>Berikut hasil pencarian</h1>
+</div>
+
+<div class="card"
 @foreach ($data as $report)
     <li>
         {{ $report->laporan }}
         <a href="/reports/{{ $report->id }}">Lihat Selengkapnya</a>
     </li>
+
+</div>
 @endforeach
 @endsection
