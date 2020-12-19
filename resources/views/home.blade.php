575520032069caa85bf5b10bd924cfbc7a75e6cb
ribkajsidabutar <ribkajsidabutar@gmail.com>
2020-12-19 19:23:12 +0700

    menambahkan background view home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Lapor</title>
     <style>
+    body{
+    background: #F08080;
+    }
     h1{
         text-align: center;
     }
