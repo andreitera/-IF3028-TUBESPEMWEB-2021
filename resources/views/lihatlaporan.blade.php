45a0bfc11e50e897fbe0512b318c53b302f45436
risnoputri07 <risnoputri2@gmail.com>
2020-12-19 18:46:42 +0700

    membuat tampilan untuk lihat laporan selengkapnya

--- resources/views/lihatlaporan.blade.php
+++ resources/views/lihatlaporan.blade.php
@@ -4,23 +4,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
+
+<style>
+.header{
+        text-align : center;
+        font-size : 24px;
+
+    }
+
+    .textlaporan{
+        width : 85%;
+        display : inline-block;
+    }
+    .kaki{
+        margin-top: 40px;
+        display: inline-block;
+    }
+    .kaki p{
+        display: inline;
+        margin-right: 20px;
+        
+    }
+    .btn{
+        display: inline;
+        float: right;
+        margin-left: 20px;
+    }
+    .button{
+        margin-top: 40px;
+        width: 200px;
+        display: inline-block;
+        float: right;
+    }
+    .lampiran{
+        display: block;
+    }
+    </style>
+
+    </head>
+    <body>
+        <div class="header">
+            <h1>SIMPLE LAPOR!!</h1>
+        </div>
+        <div class="conten">
+        <p class="textlaporan"> Buat Laporan/Komentar</p>
+        <hr><br>
+
     @if(session('pesan'))
     {{ session('pesan')}}
     @endif
     <p>{{$lengkap->pengirim}}</p>
     <p>{{$lengkap->judul}}</p>
     <p>{{$lengkap->isilaporan}}</p>
-    <p>{{$lengkap->aspect}}</p>
-    <a href="{{ $lengkap->file }}">lihat lampiran</a>
+    <a class="lampiran" href="{{ $lengkap->file }}">lihat lampiran</a>
+    
+    <div class="kaki">
+    <p>Waktu : {{$lengkap->created_at}}</p>
+    <p>Aspek : {{$lengkap->aspect}}</p>
+    </div >
+    <span class="button">
     <form action="{{ route('edit', $lengkap->id) }}" method="POST">
     @csrf
-    <button type="submit">edit</button>
+
+    <button class="btn" type="submit">edit</button>
     </form>
     <form action="{{ route('hapus', $lengkap->id) }}" method="POST">
     @csrf
-    <button type="submit">hapus</button>
+    <button class="btn" type="submit">hapus</button>
     </form>
+    </span>
+    <hr>
 </body>
 </html>
