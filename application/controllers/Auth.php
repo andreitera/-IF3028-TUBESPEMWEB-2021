1cea32db407abb6de717650c24edfe534f80d3ec
ytn70 <yerikothomasn@gmail.com>
2020-12-19 21:06:27 +0700

    mengubah routes dan menambahakan file Auth.php

---
+++ application/controllers/Auth.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+    public function index()
+    {
+        echo "test apakah terhubung";
+    }
+}
