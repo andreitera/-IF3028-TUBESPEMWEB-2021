e8dbe02e5084fa27c9789d894a4821f758b41cbc
ytn70 <yerikothomasn@gmail.com>
2020-12-19 21:45:44 +0700

    menambahkan tampilan dasar home

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -13,6 +13,41 @@
         <div class="searchBar">
             <input type="text"><button>Cari</button>
         </div>
+        <div class="buatLaporan">
+            Buat Laporan/Komentar
+            <button>tambah</button>
+        </div>
+        <p>Laporan/Komentar Terakhir</p>
+        <hr>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Quaerat maxime, natus tempore a ex expedita adipisci vel
+            nulla fuga necessitatibus, distinctio facere repellat eos
+            quo assumenda maiores fugit. Exercitationem, illum.
+        </p>
+        <div class="footerPost">
+            <div class="lampiran">
+                Lampiran:
+            </div>
+            <div class="waktu">
+                Waktu:
+            </div>
+            <div class="selengkapnya">
+                <button>Lihat Selengkapnya ></button>
+            </div>
+        </div>
+        <hr>
+        <div class="footer">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
     </div>
 </body>
 
