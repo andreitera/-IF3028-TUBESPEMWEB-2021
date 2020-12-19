a621b6198fdfce250daf16b74f57528ac26f8513
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-19 22:06:58 +0700

    add link css

--- resources/views/lihatlaporan.blade.php
+++ resources/views/lihatlaporan.blade.php
@@ -1,33 +1,25 @@
-<!DOCTYPE html>
-<html>
+@extends('Master.master')
 
-<head>
-    <title>Lihat Laporan</title>
-    <link rel="stylesheet" href=" {{ asset('css/style.css') }} ">
-</head>
-
-<body>
-    <div class="header">
-        <header style="text-align: center;margin: 50px;">
+@section('content')
             <h1>IF SIAGA LAPOR!</h1>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP1NcsHf_auML9glkHz5cIuBjXgz4m_0Mw7Q&usqp=CAU.jpg" alt="Informatika" width="400px" height="150px">
         </header>
     </div>
     <div class="head">
-        Detail Laporan/Komentar
+        <h3>Detail Laporan/Komentar</h3>
     </div>
     <hr>
-    <p>{{$data->pengirim}}</p>
-    <p>{{$data->judul}}</p>
-    <p>{{$data->laporan}}</p>
-    <p>{{$data->aspek}}</p>
-    <p>{{$data->lampiran}}</p>
-    <p>{{$data->created_at}}</p>
+    <p><b>Pengirim : </b>{{$data->pengirim}}</p>
+    <p><b>Judul Laporan : </b>{{$data->judul}}</p>
+    <p><b>Laporan : </b>{{$data->laporan}}</p>
+    <p><b>Aspek : </b>{{$data->aspek}}</p>
+    <p><b>Lampiran : </b>{{$data->lampiran}}</p>
+    <p><b>Laporan dibuat pada : </b>{{$data->created_at}}</p>
     
 
     <form action="{{ route('edit', $data->id) }}" method="POST">
         @csrf
-        <button type="submit">Edit Laporan/Komentar</button>
+        <button type="submit">Edit Laporan/Komentar</button><br>
     </form>
 
     <form action="{{ route('delete', $data->id) }}" method="POST">
@@ -37,4 +29,4 @@
     <hr>
 </body>
 
-</html>
+@endsection
