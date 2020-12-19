b0e47f73324f006af46e70394fbe65aceb1f0d3e
mizaldifathoni <55985698+mizaldifathoni@users.noreply.github.com>
2020-12-19 20:52:02 +0700

    Create tubesweb.sql

---
+++ 14116074-MizaldiFathoni/tubesweb.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Des 2020 pada 14.50
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tubesweb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lapor`
+--
+
+CREATE TABLE `lapor` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `judul` varchar(50) NOT NULL,
+  `laporan` text NOT NULL,
+  `aspek` varchar(20) NOT NULL,
+  `file` blob NOT NULL,
+  `waktu` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `lapor`
+--
+ALTER TABLE `lapor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `lapor`
+--
+ALTER TABLE `lapor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
