296027dc26166d61aa80031f4752870f5aac1872
ryznocaHub <fikrihalim27@gmail.com>
2020-12-19 10:25:10 +0700

    Form Edit LAPOR!

--- resources/views/lapor/editlaporan.blade.php
+++ resources/views/lapor/editlaporan.blade.php
@@ -1,44 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('master.master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lapor</title>
-</head>
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+@endsection
 
-<body>
-    <form method="POST" action="{{ route('laporupdate', $lapor->unique_id) }}" enctype="multipart/form-data">
-        @csrf
-        <input type="hidden" name="name" value="{{$lapor->unique_id}}">
-        <label for="">
-            Nama:
-            <input type="text" name="name" value="{{$lapor->from}}">
-        </label>
-        <label for="" style="display: block;">
-            Judul:
-            <input type="text" name="title" value="{{$lapor->title}}">
-        </label>
-        <label for="" style="display: block;">
-            Content:
-            <textarea name="content" rows="4" column="50">{{$lapor->content}}</textarea>
-        </label>
-        <label for="" style="display: block; margin-top: 20px;">
-            Aspect:
-            <select name="aspect" id="">
-                <option value="Dosen">Dosen</option>
-                <option value="Staff">Staff</option>
-                <option value="Mahasiswa">Mahasiswa</option>
-                <option value="Infrastruktur">Infrastruktur</option>
-                <option value="Pengajaran">Pengajaran</option>
-            </select>
-        </label>
-        <label for="">
-            File
-            <input type="file" name="filelapor" value="{{$lapor->file}}">
-        </label>
-        <button type="submit">Edit lapor</button>
+@section('isi')
+<div class="judul">
+    <div class="judul-kata1">Layanan Aspirasi dan Pengaduan Online Teknik Informatika ITERA</div>
+    <div class="judul-kata2">Sampaikan Laporan Anda Langsung Kepada Pihak Terkait</div>
+    <div class="judul-bar"></div>
+</div>
+<div class="form-buat">
+        <form method="POST" action="{{ route('laporupdate', $lapor->unique_id) }}" enctype="multipart/form-data" class="form">
+        Edit Laporan/Komentar
+        <div class="bar"></div>
+        <input type="hidden" name="name" value="{{ $lapor->unique_id }}">
+        <textarea id="laporan" name="laporan">{{$lapor->content}}</textarea>
+        <textarea id="laporan" name="laporan">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio voluptatem voluptatum placeat ex consequuntur vero iusto. Magnam, ratione! Nostrum eius sunt esse repudiandae dignissimos cupiditate dicta vel, consequatur tempora, omnis quia? Aut, distinctio architecto iure enim assumenda et exercitationem eligendi est, laborum aspernatur quasi laudantium dolorum autem! Illo nesciunt rem vero quod similique eligendi, porro veniam id explicabo optio. Quis, ad magni? Laborum est maxime sunt voluptatem impedit deserunt necessitatibus ut numquam animi veniam architecto, quia dolorum ipsam minima fuga velit? Autem, dignissimos illo natus incidunt molestias magni deserunt laborum sed voluptatem tempore doloremque nobis quo enim blanditiis quam odit?</textarea>
+        <select name="aspek" id="aspek">
+            <option value="Dosen">Dosen</option>
+            <option value="Mata Kuliah">Mata Kuliah</option>
+            <option value="Prodi">Prodi</option>
+        </select>
+        <br>
+        <input type="button" value="Choose File" onclick="document.getElementById('pic').click()" class="btn">
+        <input type="text" id="filename" value="No File Choosen">
+        <input type="file" id="pic" name="pic" style="display:none" onchange="document.getElementById('filename').value=this.value">
+        <div class="container">
+            <button id="batal" class="btn" style="margin-right: 20px">Batal</button>
+            <button id="submit" class="btn">Ubah LAPOR!</button>
+        </div>
+        <div class="bar"></div>
     </form>
-</body>
-
-</html>
+</div>
+@endsection
