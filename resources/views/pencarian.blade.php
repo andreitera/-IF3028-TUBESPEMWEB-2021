32e380fc37294e93b5e8d88a235cee18dd5c717c
nazla118140084 <nazla.118140084@student.itera.ac.id>
2020-12-19 22:07:18 +0700

    add link css

--- resources/views/pencarian.blade.php
+++ resources/views/pencarian.blade.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hasil Pencarian</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href=" {{ asset('css/style.css') }} ">
-</head>
-<body>
+@extends('Master.master')
 
+@section('content')
 <h1>Berikut hasil pencarian</h1>
 
-    @foreach ($data as $report)
-        <li>
-            {{ $report->laporan }}
-            <a href="/reports/{{ $report->id }}">Lihat Selengkapnya</a>
-        </li>
-    @endforeach
-</body>
-</html>
+@foreach ($data as $report)
+    <li>
+        {{ $report->laporan }}
+        <a href="/reports/{{ $report->id }}">Lihat Selengkapnya</a>
+    </li>
+@endforeach
+@endsection
