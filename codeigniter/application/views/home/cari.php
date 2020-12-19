5550386056cbf8710c340b4af1435feb26177a35
Your Name <ardimanalu02@gmail.com>
2020-12-19 19:31:54 +0700

    perbaharui tampilan dan program

--- codeigniter/application/views/home/cari.php
+++ codeigniter/application/views/home/cari.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<ul>
+<<<<<<< HEAD <ul>
 	<?php foreach ($lapor as $value) { ?>
 		<!--$lapor didapet dari controller home -->
 		<li>
@@ -12,19 +11,5 @@
 		<br>
 		<hr />
 	<?php } ?>
-=======
-<ul>
-	<?php foreach ($lapor as $value) { ?>
-		<!--$lapor didapet dari controller home -->
-		<li>
-			<h4 style="margin-bottom: 20px;"><?php echo substr($value["isi"], 0, 100); ?>.....</h4>
-			<h4 class="lampiran">Lampiran : <?php echo $value["file"]; ?></h4>
-			<a href="<?php echo base_url() ?>index.php/Home/detail/<?php echo $value["id"] ?>""><h4 class=" detail">Selengkapnya >>></h4></a>
-			<h4 class="waktu" style="margin-right: 10px;">Waktu : <?php echo $value["tanggal"]; ?></h4>
-			<div style="clear: both"></div>
-		</li>
-		<br>
-		<hr />
-	<?php } ?>
->>>>>>> 39462ae4bc22c375153b367965504090af312017
-</ul>
+
+	</ul>
