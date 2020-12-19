be83f40443b5b657d551c9d23e98fe5c7be1c1d4
muhkhoirurrizqi <mkhoirurrizqi08@gmail.com>
2020-12-19 16:31:19 +0700

    fix bug

--- public/assets/js/validation.js
+++ public/assets/js/validation.js
@@ -3,7 +3,7 @@ function validasi(){
     const aspek = document.getElementById('aspek').value;
     const filename = document.getElementById('input-lampiran').value;
 
-    const splitedWord = konten.value.split(" ");
+    const splitedWord = konten.value.split("");
     if(splitedWord.length < 20){
         alert("Laporan setidaknya terdiri dari 20 karakter!");
         return false;
@@ -21,11 +21,10 @@ function validasi(){
     
     else if(filename != ""){
         let splitname = filename.split(".");
-        let fileExt = splitname[splitname.length-1]; 
         let extension = ['doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'jpg', 'jpeg', 'png'];
         let same = 0
         for(i = 0 ; i < extension.length ;i++){
-            if(splitname[1] == extension[i]){
+            if(splitname[extension.length-1] == extension[i]){
                 same = 1;
             }
         }
