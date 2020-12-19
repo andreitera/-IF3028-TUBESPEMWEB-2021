7c003a847d89202852ce859a4fa9705b9008c3ac
fitriandanis <65114159+fitriandanis@users.noreply.github.com>
2020-12-19 21:50:02 +0700

    Menambahkan file detail_laporan.php

---
+++ application/views/detail_laporan.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Detail Laporan</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<div class="pembungkus">
+		<header>SIMPLE LAPOR!</header>
+
+		<div class="konten">
+			<p>Detail Laporan/Komentar</p>
+			<hr><br>
+			<?php
+			foreach ($data as $key) {
+				echo $key['laporan'];
+				?>
+
+				<p>
+					Lampiran<br>
+					<?php
+						echo $key['file'];
+						?>
+				</p>
+				<div class="lampiran">
+					Waktu:
+					<?php
+						echo $key['waktu'];
+						?>
+					Aspek:
+					<?php
+						echo $key['aspek'];
+						?>
+				</div>
+				<div class="selengkapnya">
+					Hapus Laporan/Komentar
+					<a href="<a href='http://localhost/-IF3028-TUBESPEMWEB-2021-master/-IF3028-TUBESPEMWEB-2021-master/CodeIgniter-3.1.11/index.php/Welcome/HapusLaporan/ <?php $key['id'] ?> "><img src="img/close.png"></a>
+				</div>
+				<hr>
+			<?php } ?>
+		</div>
+	</div>
+</body>
+
+</html>
