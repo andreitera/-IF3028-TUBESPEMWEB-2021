cd5ae1af688624141d9a1d59d2cae693cae27135
Muhammad Najie <57042601+muhammadnajie@users.noreply.github.com>
2020-12-19 20:11:38 +0700

    fix bug create

--- app/Views/create.php
+++ app/Views/create.php
@@ -1,7 +1,7 @@
 <main>
     <div class="container">
         <h3 class="pt-2">Buat Laporan/Komentar</h3>
-        <form onsubmit="return validasi()" action="save" method="POST" class="main-form" id="form" enctype="multipart/form-data">
+        <form onsubmit="return validasi()" action="/laporan/save" method="POST" class="main-form" id="form" enctype="multipart/form-data">
             <textarea name="konten" id="konten" class="mb-1" 
                         placeholder="Laporan/Komentar"></textarea>
             <select name="aspek" id="aspek" class="mb-1">
