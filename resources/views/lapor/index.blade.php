7ef4bb02bd8a2a3ea60d1147404b4975759b3da4
ryznocaHub <fikrihalim27@gmail.com>
2020-12-19 15:48:29 +0700

    icon search

--- resources/views/lapor/index.blade.php
+++ resources/views/lapor/index.blade.php
@@ -10,30 +10,27 @@
     <div class="judul-kata2">Sampaikan Laporan Anda Langsung Kepada Pihak Terkait</div>
     <div class="judul-bar"></div>
 </div>
-<div class="form-buat">
-        <form method="POST" action="{{ route('laporbuat') }}" enctype="multipart/form-data" class="form">
-        Buat Laporan/Komentar
-        {{-- <div class="form-container">
-            <div class="form-kategori">Prodi</div>
-            <div class="form-kategori">Dosen</div>
-            <div class="form-kategori">Mata Kuliah</div>
-        </div> --}}
+    <div class="form-buat">
+        <div style="display: flex; justify-content:center">
+            <input type="text" name="judul" id="judulsearch">
+            <button class="btn-search"><img src="{{ asset('image/search-black.svg') }}" alt="icon search" class="search">Cari</button>
+        </div>
+        <form method="POST" action=" route('searchlapor') " enctype="multipart/form-data" class="form">
+        <div class="text-detail" style="margin-bottom: 40px">Buat Laporan/Komentar <img src="{{ asset('image/plus.svg') }}" alt="icon plus"></div>
+        Laporan/Komentar Terakhir
         <div class="bar"></div>
-        <textarea id="laporan" name="laporan" placeholder="Laporan/Komentar"></textarea>
-        <select name="aspek" id="aspek">
-            <option value="" disabled selected>Pilih Aspek Pelaporan/Komentar</option>
-            <option value="Dosen">Dosen</option>
-            <option value="Mata Kuliah">Mata Kuliah</option>
-            <option value="Prodi">Prodi</option>
-        </select>
-        <br>
-        <input type="button" value="Choose File" onclick="document.getElementById('pic').click()" class="btn">
-        <input type="text" id="filename" value="No File Choosen">
-        <input type="file" id="pic" name="pic" style="display:none" onchange="document.getElementById('filename').value=this.value">
-        <div class="container">
-            <button id="submit" class="btn">Buat LAPOR!</button>
+        {{-- satu laporan --}}
+        <div class="detail">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat deserunt natus ipsum rerum ad corporis, a adipisci? Debitis iure dignissimos quae a culpa nemo commodi? Eum cumque aliquam doloremque facere, adipisci veniam sed dignissimos maxime obcaecati tempora, iure consectetur. Sed, repellendus aperiam? Odit architecto autem recusandae officiis! Laudantium obcaecati tempora magnam at quia cumque illum quidem totam, amet sint sit quas nobis pariatur corporis veniam eligendi dolores vitae ea et deleniti nam eaque modi dolorum. Dolores doloremque, saepe eius mollitia sapiente, odit optio quibusdam possimus repudiandae sit modi reprehenderit quas at voluptatum sunt dolor! Pariatur modi assumenda vitae odit commodi?</div>
+        <div class="form-container">
+            <span class="form-container-aspek">Lampiran: Gambar.Jpg</span>
+            <span style="margin-right: 20px !important">Waktu: 20-11-2019 20:00</span>
+            <span class="f-center">Lihat Selengkapnya<img src="{{ asset('image/right-arrow.svg') }}" alt="panah kanan" class="icon-bawah" style="margin-left:2px"></span>
         </div>
         <div class="bar"></div>
-    </form>
+        {{-- end satu laporan --}}
+        <div class="f-center" style="margin-top: 30px">
+            <img src="{{ asset('image/titik3.svg') }}" alt="icon titik3" style="height: 25px">
+        </div>
+    </div>
 </div>
 @endsection
