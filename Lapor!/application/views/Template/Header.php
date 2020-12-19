a4a137b93c1e5e4b40effe0329f8686b597f3961
sipangkar1d <45484386+sipangkar1d@users.noreply.github.com>
2020-12-19 22:40:23 +0700

    Update Header.php
    menambah img logo itera pada title

--- Lapor!/application/views/Template/Header.php
+++ Lapor!/application/views/Template/Header.php
@@ -6,7 +6,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" tyle = "text/css" href="<?= base_url();?>css/style.css">
         <script scr="<?= base_url();?>js/script.js"></script>
-        <title>Lapor ITERA</title>        
+        <title>Lapor ITERA</title> 
+        <link rel="shortcut icon" type="image/x-icon" href="https://1.bp.blogspot.com/-IC3Mc1nmQ-0/XxhVTV1xGTI/AAAAAAAACxM/Q9QlL_EpvgkpxKxqACtWkK4xCzVAj8eogCLcBGAsYHQ/s1600/LOGO%2BITERA%2BPNG%2BHD.png" />       
     </head>
     <body>
     <div class="container">
