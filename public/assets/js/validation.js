9eb953ae7a83f25423d029a84b9413d503ffc05e
Muhammad Najie <57042601+muhammadnajie@users.noreply.github.com>
2020-12-19 20:25:50 +0700

    validasi

--- public/assets/js/validation.js
+++ public/assets/js/validation.js
@@ -3,37 +3,32 @@ function validasi(){
     const aspek = document.getElementById('aspek').value;
     const filename = document.getElementById('input-lampiran').value;
 
-    const splitedWord = konten.value.split("");
+    const splitedWord = konten.value.split(" ");
     if(splitedWord.length < 20){
-        alert("Laporan setidaknya terdiri dari 20 karakter!");
+        alert("Laporan setidaknya terdiri dari 20 kata!");
         return false;
     }
     
-    else if(aspek == 0){
+    if(aspek == 0){
         alert("Pilih aspek terlebih dahulu!");
         return false;
     }
 
-    else if(filename == ""){
+    if(filename == ""){
         alert("Masukkan lampiran terlebih dahulu");
         return false;
     }
     
-    else if(filename != ""){
-        let splitname = filename.split(".");
-        let extension = ['doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'jpg', 'jpeg', 'png'];
-        let same = 0
-        for(i = 0 ; i < extension.length ; i++){
-            if(splitname[splitname.length-1] == extension[i]){
-                same = 1;
-            }
-        }
-        if(same == 0){
+    if(filename != ""){
+        const allowed_extension = [
+            'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'jpg', 'jpeg', 'png'
+        ];
+        const dot = filename.lastIndexOf(".");
+        const ext = filename.slice(dot+1);
+        const ada = allowed_extension.some(item => item == ext);
+        if (!ada) {
             alert("Ekstensi file tidak didukung!");
             return false;
-        }
-        else{
-            return true;
         }
     }
 }
