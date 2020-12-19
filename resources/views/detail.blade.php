d008bd4219ea473824c83f78da8204c5a644e82b
anastasiaputeri00 <anastasiaputerid@gmail.com>
2020-12-19 18:04:17 +0700

    view detail

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+.margin {
+    margin: auto 100px auto 100px;
+}
+
+.waktu {
+    float: left;
+    width: 20%;
+}
+
+.aspek {
+    float: left;
+    width: 20%;
+    text-align: left;
+}
+
+.hapus {
+    float: left;
+    width: 60%;
+    text-align: right;
+}
+
+.clearfix {
+    clear: both;
+}
+</style>
+<body>
+    <br><br>
+    <div class="margin">
+        <center><h1>SIMPLE LAPOR!</h1></center>
+        <br><br><br>
+
+        <p>Detail Laporan/Komentar</p>
+        <hr><br>
+        
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Rerum, culpa minus! Odio quidem delectus aliquam, consectetur
+        deserunt possimus ipsum corrupti in reprehenderit deleniti illum,
+        nostrum voluptates mollitia at modi ducimus. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Voluptate inventore est,
+        nisi magnam ab sed commodi, vitae laboriosam voluptates soluta voluptatum
+        debitis praesentium recusandae repellat impedit quae consectetur assumenda.
+        Quidem.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Rerum, culpa minus! Odio quidem delectus aliquam, consectetur
+        deserunt possimus ipsum corrupti in reprehenderit deleniti illum,
+        nostrum voluptates mollitia at modi ducimus. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Voluptate inventore est,
+        nisi magnam ab sed commodi, vitae laboriosam voluptates soluta voluptatum
+        debitis praesentium recusandae repellat impedit quae consectetur assumenda.
+        Quidem.</p> <br>
+
+        <p>Lampiran: </p><br>
+
+        <img src="kotak.png" alt="">
+
+            <div class="waktu">
+                <p>Waktu: 20-11-2019 20:00</p>
+            </div>
+
+            <div class="aspek">
+                <p>Aspek: Infrastruktur</p>
+            </div>
+
+            <div class="hapus">
+                <p>Hapus Laporan/Komentar x</p>
+            </div>
+
+            <div class="clearfix"></div>
+            <hr>
+    </div>
+</body>
+</html>
