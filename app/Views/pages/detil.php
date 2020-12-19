88aba4b188685e6996e6ff1847fd27034831bc8a
Yuni Nurdiyanti <yuni.118140044@student.itera.ac.id>
2020-12-19 10:59:46 +0000

    styling detail laporan

--- app/Views/pages/detil.php
+++ app/Views/pages/detil.php
@@ -1,76 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lapor.go.id</title>
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>/css/detil.css">
 </head>
-<style>
-    * {
-        box-sizing: border-box;
-    }
-
-    body {
-        background-color: #eaeaea;
-        color: #333333;
-    }
-
-    .container {
-        margin-left: auto;
-        margin-right: auto;
-        padding-left: 30px;
-        padding-right: 30px;
-        width: 100% !important;
-        background-color: white;
-        position: relative;
-    }
-
-    .header {
-        font-size: 24pt;
-        font-style: Calibri;
-    }
-
-    .container img {
-        width: 200px;
-        height: 200px;
-        display: block;
-    }
-
-    .footer {
-        padding: 10px;
-    }
-
-    .terakhir {
-        float: right;
-        margin-left: 15px;
-    }
-</style>
-
-<body>
-    <div class="container">
-        <div class="header">
-            <center>
-                <h1>
-                    SIMPLE LAPOR
-                </h1>
-            </center>
-        </div>
-        <P>Detail Laporan/Komentar</P>
+<body class="body">
+    
+    <div class="header">
+            <h1 class="h1">SIMPLE LAPOR! </h1>
+    </div>
+        <P><b>Detail Laporan/Komentar</b></P>
         <hr>
-        <div class="conten">
-            <p><?= $lapor['isi']; ?></p>
-            <p>Lampiran</p>
-            <img src="<?= base_url(); ?>/gambar/<?= $lapor['lampiran'] ?>" alt="Avatar">
-        </div>
-        <div class="footer">
-            <span>Waktu : <?= $lapor['create_at'] ?></span>
-            <span>Aspek : <?= $lapor['aspek'] ?></span>
+        <p class=""><?= $lapor['isi']; ?></p>
+        <p>Lampiran</p>
+        <div>
+        <img src="<?= base_url(); ?>/gambar/<?= $lapor['lampiran'] ?>" alt="Avatar">
+        </div>       
+        <div class="foot">
+            <span>Waktu : 20-11-2019 20:00</span>
+            <br>
+            <span>Aspek: Infrasturktur</span>
+            <br><br>
             <span class="terakhir"><a href="<?= base_url(); ?>/Lapor/delete/<?= $lapor['id'] ?>">Hapus Laporan &#10006</a></span>
-            <span class="terakhir"><a href="<?= base_url(); ?>/Lapor/edit/<?= $lapor['id'] ?>">Ubah</a></span>
+            <span class="terakhir"><a href="<?= base_url(); ?>/Lapor/edit/<?= $lapor['id'] ?>">Ubah </a></span>
             <hr>
         </div>
-    </div>
+
+       
 </body>
 
 </html>
