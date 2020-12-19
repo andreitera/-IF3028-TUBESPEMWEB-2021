ce14b420ce72f3510b3c3d309948812a53bd80ed
vinaoktariana <vinaoktariana3@gmai.com>
2020-12-19 23:25:47 +0700

    tampilan semua laporan

--- resources/views/semualaporan.blade.php
+++ resources/views/semualaporan.blade.php
@@ -1,11 +1,17 @@
 @extends('Master.master')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/styleSemuaLaporan.css') }}">
+@endsection
+
 @section('content')
-<h1>Tampilkan Laporan</h1>
+<header style="text-align: center;margin: 50px;">
+    <h1 clas="card">Tampilkan Laporan</h1>
+</header>
 
     <ul>
         @foreach ($data as $data)
-        <li>
+        <li class="card">
             {{ $data->laporan }}
             <a href="{{ route('show', $data->id) }}">Lihat Selengkapnya</a>
         </li>
