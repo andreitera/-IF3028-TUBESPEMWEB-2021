0719c36f815b609c4518a2e7b5b3171455c4f703
sipangkar1d <45484386+sipangkar1d@users.noreply.github.com>
2020-12-19 21:30:30 +0700

    Update Beranda.php
    merubah view search

--- Lapor!/application/views/Pages/Beranda.php
+++ Lapor!/application/views/Pages/Beranda.php
@@ -1,14 +1,19 @@
     <div class="search-bar" >
-        <form class="example" action="action_page.php">
+        <!-- <form class="example" action="action_page.php">
             <input type="text" placeholder="Search.." name="search">
             <button type="submit"><i class="fa fa-search"></i>Cari</button>
-        </form>	
+        </form>	 -->
+        <form class="searchbar" action="<?php echo base_url('laporan/search') ?>" method="GET">
+            <input type="search" name="keyword" id="search" placeholder="Cari.....">
+            <input type="submit" class="hidden" value="&#128269 Cari">
+        </form>
     </div>
-    <br>
-    <div class="buat-laporan">
-        <a href="<?php echo site_url().'laporan/add/'; ?>" style="color:black">Buat Laporan/Komentar <i class="fa fa-plus-square"></i></a>
-    </div>    
     <br><br>
+    <div class="buat-laporan">
+    <br>
+        <a href="<?php echo site_url().'laporan/add/'; ?>" style="color:black"><b>Buat Laporan/Komentar </b><i class="fa fa-plus-square"></i></a>
+    </div>    
+    <br>
     <div class="tampil-database">
         <p><b>Laporan/Komentar terakhir</b></p><hr>         
         
