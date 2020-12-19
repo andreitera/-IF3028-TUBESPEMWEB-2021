a2c2d6c17e9fe7b566650ad20844c3b2ecaf404e
Muhammad Najie <57042601+muhammadnajie@users.noreply.github.com>
2020-12-19 11:27:29 +0700

    memecah validasi menjadi file

--- app/Views/create.php
+++ app/Views/create.php
@@ -1,7 +1,7 @@
 <main>
     <div class="container">
         <h3 class="pt-2">Buat Laporan/Komentar</h3>
-        <form onsubmit="return validasi()"action="save" method="POST" class="main-form" id="form" enctype="multipart/form-data">
+        <form action="save" method="POST" class="main-form" id="form" enctype="multipart/form-data">
             <textarea name="konten" id="konten" class="mb-1" 
                         placeholder="Laporan/Komentar" no></textarea>
             <select name="aspek" id="aspek" class="mb-1">
@@ -18,40 +18,4 @@
         </form>
     </div>
 </main>
-<script>
-    function validasi(){
-        let konten = document.getElementById('konten')
-        let aspek = document.getElementById('aspek').value;
-        let filename = document.getElementById('input-lampiran').value;
-        if(konten.value.length < 20){
-            alert("Laporan setidaknya terdiri dari 20 karakter!");
-            return false;
-        }
-        else if(aspek == 0){
-            alert("Pilih aspek terlebih dahulu!");
-            return false;
-        }
-        else if(filename == ""){
-            alert("Masukkan lampiran terlebih dahulu");
-            return false;
-        }
-        else if(filename != ""){
-            let splitname = filename.split(".");
-            let fileExt = splitname[splitname.length-1]; 
-            let extension = ['doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'jpg', 'jpeg', 'png'];
-            let same = 0
-            for(i = 0 ; i < extension.length ;i++){
-                if(splitname[1] == extension[i]){
-                    same = 1;
-                }
-            }
-            if(same == 0){
-                alert("Ekstensi file tidak didukung!");
-                return false;
-            }
-            else{
-                return true;
-            }
-        }
-    }
-</script>
+<script src="/assets/js/validation.js"></script>
---
+++ public/assets/js/validation.js
@@ -0,0 +1,46 @@
+const btnSave = document.getElementById('tambah-laporan');
+btnSave.addEventListener('click', function(e) {
+    e.preventDefault();
+    validasi()
+});
+
+function validasi(){
+    const konten = document.getElementById('konten');
+    const aspek = document.getElementById('aspek').value;
+    const filename = document.getElementById('input-lampiran').value;
+
+    const splitedWord = konten.value.split(" ");
+    if(splitedWord.length < 20){
+        alert("Laporan setidaknya terdiri dari 20 karakter!");
+        return false;
+    }
+    
+    else if(aspek == 0){
+        alert("Pilih aspek terlebih dahulu!");
+        return false;
+    }
+
+    else if(filename == ""){
+        alert("Masukkan lampiran terlebih dahulu");
+        return false;
+    }
+    
+    else if(filename != ""){
+        let splitname = filename.split(".");
+        let fileExt = splitname[splitname.length-1]; 
+        let extension = ['doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'jpg', 'jpeg', 'png'];
+        let same = 0
+        for(i = 0 ; i < extension.length ;i++){
+            if(splitname[1] == extension[i]){
+                same = 1;
+            }
+        }
+        if(same == 0){
+            alert("Ekstensi file tidak didukung!");
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
