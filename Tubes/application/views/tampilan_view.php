e83498958b04ca0acf66e9249571a5b0885ca106
RinggoGalihSadewo <ringgo.118140076@student.itera.ac.id>
2020-12-19 12:20:41 +0700

    Debugging View

--- Tubes/application/views/tampilan_view.php
+++ Tubes/application/views/tampilan_view.php
@@ -20,73 +20,45 @@
 		</div>
 
 		<form action="" method="POST">
+			
 			<center>
-			<input type="text" name="cari">
-			<button type="submit" name="btncari">Cari</button>
+			
+				<input type="text" name="cari">
+			
+				<button type="submit" name="btncari">Cari</button>
+			
 			</center>
+
 		</form>
 
 		<br>
+
 		<center>
-		<a href="<?php  echo site_url('Laporan_controller/tambah') ?>">Buat Laporan/Komentar</a>
+		
+			<a href="<?php  echo site_url('Laporan_controller/tambah') ?>">Buat Laporan/Komentar</a>
+		
 		</center>
 		
 
 		<br>
+		
+			<p>Laporan/Komentar Terakhir</p>
+		
 		<hr>
 
-		<?php 
-		foreach ($laporan as $value):
+		<?php foreach ($laporan as $value):
+			 
 			 echo "<p>$value->isi</p><br>";
 			 echo "<p>Lampiran: $value->lampiran</p>";  
 			 echo "<p style='width: auto; height: auto; float: right; margin-top:-35.9px; margin-right: 150px;'>Waktu: $value->waktu</p>";
-		?>
-			<a href="<?php echo site_url('Laporan_controller/detail') ?>" style="float: right; margin-top:-35.9px;">Lihat Selengkapnya</a>
-			<hr>
-		<?php  
-		 endforeach; ?>
 		
-			<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-
-			<p>Lampiran File: Nama File </p>
-
-			<div class="detail">Tampilan Waktu <a href="<?php  echo site_url('Laporan_controller/detail') ?>">Lihat Selengkapnya</a></div>
-			
-
+		?>
+		
+		<a href="<?php echo site_url('Laporan_controller/detail') ?>" style="float: right; margin-top:-35.9px;">Lihat Selengkapnya</a>
+		
 		<hr>
-			<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-			
-			<p>Lampiran File: Nama File </p>
-
-			<div class="detail">Tampilan Waktu <a href="<?php  echo site_url('Laporan_controller/detail') ?>">Lihat Selengkapnya</a></div>
-
-		<hr>
-			<p>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
-
-			<p>Lampiran File: Nama File </p>
-
-			<div class="detail">Tampilan Waktu <a href="<?php  echo site_url('Laporan_controller/detail') ?>">Lihat Selengkapnya</a></div>
+		
+		<?php endforeach; ?>
 
 		</div>
 
