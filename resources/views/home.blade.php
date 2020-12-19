dc8d5442427645113e4756b69864ee26d743a060
risnoputri07 <risnoputri2@gmail.com>
2020-12-19 14:55:27 +0700

    Ubah tampilan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,25 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Lapor</title>
+    <style>
+    h1{
+        text-align: center;
+    }
+    .kaki{
+        margin-left: 20px;
+    }
+    .kanan{
+        float : right;
+    }
+    </style>
 </head>
 <body>
+<h1>SIMPLE LAPOR!!</h1>
     <a href="{{route('daftarlaporan')}}">lihat list laporan</a>
     <a href="{{route('buatlaporan')}}">buat laporan</a>
     <form action="{{ route('cari') }}" method="POST">
         @csrf
-        <label for="">
+        
         cari:
         <input type="text" name="cari">
         </label>
         <button type="submit">cari laporan</button>
     </form>
-    <table border="1">
-    @foreach ($data as $data)
-    <tr>
-        <td>{{$data->pengirim}}</td>
-        <td>{{$data->judul}}</td>
-    </tr>
+    <p>Laporan/Komentar Terakhir</p>
+    <hr>
+    @foreach ($data as $lapor)
+    <div class="container">
+    <div class="card">
+    <p>{{$lapor->isilaporan}}</p>
+    <div class="kaki">
+    <span>Lampiran : {{$lapor->file}}</span>
+    <span class="kanan">
+    <span>Waktu : {{$lapor->created_at}}</span>
+    <span> <a href="/laporan/{{$lapor->id}}">Lihat Selengkapnya X</a></span>
+    </span>
+    </div>
+    <hr>
+    </div>
+    </div>
     @endforeach
-    </table>
 </body>
 </html>
