31a033f785f1d1292cb463b6f2b766972ef3b65d
annisa118140082 <annisa.118140082@student.itera.ac.id>
2020-12-19 22:11:58 +0700

    halaman utama fix

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -1,25 +1,35 @@
 @extends('Master.master')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/styleHalamanUtama.css') }}">
+@endsection
+
 @section('content')
 <body>
-    <h1>SIMPLE LAPOR!</h1>
+    <div class="jumbotron">
+        <h1>SIMPLE LAPOR!</h1>
 
-    <form action="{{ route('cari') }}" method="POST">
-        @csrf
-        <label for="">
-            <input type="text" name="cari" autocomplete="off">
-        </label>
-        <button type="submit">Cari</button>
-    </form>
-    <a href="{{ url('/formlapor') }}">Buat laporan/komentar</a><br>
-    <a href="{{ url('/listlaporan') }}">Lihat Semua Laporan/Komentar</a>
+        <form action="{{ route('cari') }}" method="POST">
+            @csrf
+            <label for="">
+                <input type="text" name="cari" autocomplete="off">
+            </label>
+            <button type="submit">Cari</button>
+        </form>
 
-    <ul>
-        @foreach ($data as $report)
-        <li>
-            {{ $report->laporan }}
-            <a href="/reports/{{ $report->id }}">Lihat Selengkapnya</a>
-        </li>
-        @endforeach
-    </ul>
+        <a href="{{ url('/formlapor') }}">Buat laporan/komentar</a><br>
+        <a href="{{ url('/listlaporan') }}">Lihat Semua Laporan/Komentar</a>
+    </div>
+    <main>
+        <div>
+            <ul>
+                @foreach ($data as $report)
+                <li class="card">
+                    {{ $report->laporan }}
+                    <a href="/reports/{{ $report->id }}">Lihat Selengkapnya</a>
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </main>
     @endsection
