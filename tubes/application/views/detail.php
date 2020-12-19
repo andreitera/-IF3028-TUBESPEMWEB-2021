23642aa58284947ec0e5b5bce6aa6c0fa2f3caaf
Putrakrnwn <Putrakurniawans24@gmail.com>
2020-12-19 20:36:27 +0700

    Create detail.php

---
+++ tubes/application/views/detail.php
@@ -0,0 +1,17 @@
+<?php
+	$this->load->view('component/menu_nav');
+	$this->load->view('component/header');
+?>
+ 
+<div class="con">
+	<div class="row">
+<div class="col-md-12">
+		<div class="jumbotron">
+<img src="https://www.wearedevelopers.com/wp-content/uploads/2016/01/smart-database-driven-routing-in-codeigniter.jpg" alt="<?php echo $detail->title ?>" style="width: 100%">
+		<h1 class="display-3"><?php echo $detail->title ?></h1>
+<hr class="my-4">
+		<p class="lead"><?php echo $detail->description ?></p>
+</div>
+		</div>
+	</div>
+</div>
