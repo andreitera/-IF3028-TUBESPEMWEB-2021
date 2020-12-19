2d37297e7eddb4d47ae6d4da1c2534c91b354459
TheChosenOne29 <gilbertwuaten@gmail.com>
2020-12-19 23:57:32 +0700

    inisialisasi project

---
+++ app/Http/Controllers/demo.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class demo extends Controller
+{
+    //
+   public function home(){
+       $data = DB::table('laporan')->get() ;
+       return view('home',['data' => $data]) ;
+   }
+
+   public function input(){
+
+
+       return view('input' ) ;
+   }
+
+   public function proses(Request $request){
+       $komentar = $request->input('komentar') ;
+       $aspek    = $request->input('aspek') ;
+       $file = $request->file('file') ;
+       $waktu = date('d-m-Y H:i') ;
+       $name = $file->getClientOriginalName() ;
+
+       $file->move('file_baru' , $name) ;
+
+       DB::table('laporan')->insert([
+           'komentar' => $komentar ,
+           'aspek' => $aspek ,
+           'lampiran' => $name ,
+           'waktu' => $waktu
+       ]);
+   }
+
+   public function detail($id){
+       $data = DB::table('laporan')
+           ->where('id' , $id)
+           ->get() ;
+
+       return view('data' , ['data' => $data]) ;
+   }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','demo@home') ;
+Route::get('/input' , 'demo@input') ;
+Route::post('/prosesdata' , 'demo@proses') ;
+Route::get('/detail/{id}' , 'demo@detail') ;
