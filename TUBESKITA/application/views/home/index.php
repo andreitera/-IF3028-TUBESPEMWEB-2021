7fc2f10ddd61d8c9003866ab98065a94f3913a11
Muhammad Zulfarhan <64683758+han2512@users.noreply.github.com>
2020-12-19 11:22:32 +0700

    tambah ajax dan ubah home

--- TUBESKITA/application/views/home/index.php
+++ TUBESKITA/application/views/home/index.php
@@ -14,23 +14,25 @@
 		<input type="text" name="cari">
 		<button type="submit" name="submit">CARI</button>
 	</form>
-
-	<a href="" ><h3>Buat Keluhan +</h3></a>
-	<h4>laporan / komentar terakhir</h4>
+	<a href="" ><h3>Buat Laporan / Keluhan +</h3></a>
+	<h4>Laporan / Keluhan terakhir</h4>
 	<hr />
+	<div id="keluhan">
 	<ul>
 	<?php foreach ($lapor as $value){ ?> <!--$lapor didapet dari controller home -->
 		<li>
 			<h4 style="margin-bottom: 20px;"><?php echo $value["isi"]; ?></h4>
-			<h4 class="lampiran">lampiran : foto.jpg</h4>
-			<a href=""><h4 class="detail">selengkapnya>></h4></a>
+			<h4 class="lampiran">Lampiran : <?php echo $value["file"]; ?></h4>
+			<a href="<?php echo base_url() ?>index.php/Home/detail/<?php echo $value["id"] ?>"><h4 class="detail">selengkapnya>></h4></a>
 			<h4 class="waktu" style="margin-right: 10px;">Tanggal : <?php echo $value["tanggal"]; ?></h4>
 			<div style="clear: both"></div>
 		</li>
 		<hr />
 	<?php } ?>
 	</ul>
+	</div>
 </div>
-
+<input id="alamat" value="<?php echo base_url() ?>index.php/Home/cari/" hidden>
+<script src="<?php echo base_url() ?>/asset/ajax.js"></script>
 </body>
 </html>
