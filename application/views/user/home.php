22bfd8eb5e8a4bc8bde740fdd71ab729c511eea2
ytn70 <yerikothomasn@gmail.com>
2020-12-19 21:32:46 +0700

    menambahkan search bar

--- application/views/user/home.php
+++ application/views/user/home.php
@@ -9,7 +9,9 @@
     <div class="box">
         <div class="header">
             <h1>SIMPLE LAPOR!</h1>
-
+        </div>
+        <div class="searchBar">
+            <input type="text"><button>Cari</button>
         </div>
     </div>
 </body>
