b659026ad405c58437f6d78ba5910deb942d3d89
leo viranda <50198556+leoviranda@users.noreply.github.com>
2020-12-19 22:38:17 +0700

    update database terbaru

--- lapor/assets/lapor.sql
+++ lapor/assets/lapor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2020 at 06:14 AM
+-- Generation Time: Dec 19, 2020 at 04:37 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.2.24
 
@@ -31,10 +31,17 @@ SET time_zone = "+00:00";
 CREATE TABLE `laporan` (
   `id` int(11) NOT NULL,
   `komentar` text NOT NULL,
-  `lampiran` varchar(30) NOT NULL,
+  `lampiran` varchar(30) DEFAULT NULL,
   `aspek` varchar(30) NOT NULL,
   `waktu` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `laporan`
+--
+
+INSERT INTO `laporan` (`id`, `komentar`, `lampiran`, `aspek`, `waktu`) VALUES
+(6, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'Screenshot_(3).png', 'akademik', '2020-12-18 15:09:36');
 
 --
 -- Indexes for dumped tables
@@ -54,7 +61,7 @@ ALTER TABLE `laporan`
 -- AUTO_INCREMENT for table `laporan`
 --
 ALTER TABLE `laporan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
