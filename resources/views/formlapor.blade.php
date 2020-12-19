04388d20b0c48effaa2ca8016b1d1607d9d78692
vinaoktariana <vinaoktariana3@gmai.com>
2020-12-19 23:24:54 +0700

    form lapor

--- resources/views/formlapor.blade.php
+++ resources/views/formlapor.blade.php
@@ -1,5 +1,9 @@
 @extends('Master.master')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/styleFormLapor.css') }}">
+@endsection
+
 @section('content')
 <body>
     @if (session('msg'))
@@ -8,26 +12,30 @@
 
     <div class="container">
         <div class="content">
-            <div class="header">
+            <div class="jumbotron">
                 <header style="text-align: center;margin: 50px;">
                     <h1>IF SIAGA LAPOR!</h1>
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP1NcsHf_auML9glkHz5cIuBjXgz4m_0Mw7Q&usqp=CAU.jpg" alt="Informatika" width="400px" height="150px">
                 </header>
             </div>
             <div class="head">
+            <li class="card">
                 Buat Laporan/Komentar
+            </li>
             </div>
             <hr>
             <form method="post" action="{{ route('store')}}" enctype="multipart/form-data">
                 @csrf
-                <div>
+                
+            <li class="card">   
+                <div>       
                     Pengirim:
                     <input type="text" name="pengirim"><br>
-                </div>
+                </div><br>
                 <div>
                     Judul:
                     <input type="text" name="judul"><br>
-                </div>
+                </div><br>
                 <div class="main">
                     <textarea rows="10" cols="100" name="laporan" placeholder="Laporan/Komentar" required></textarea>
                 </div><br>
@@ -43,6 +51,7 @@
                 </div><br>
                 <div class="file">
                     <input type="file" name="lampiran" required style="display: block; padding-top: 5px;">
+            </li>
                 </div><br>
                 <div class="btn_submit">
                     <button type="submit" name="buat" style="display: block; padding-top: 5px;">Buat LAPOR!</button>
