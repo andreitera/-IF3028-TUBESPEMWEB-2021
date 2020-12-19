1860a855aa670c354c819bddfffd2d8a0ae04b70
Putrakrnwn <Putrakurniawans24@gmail.com>
2020-12-19 20:35:29 +0700

    tambah header.php

---
+++ tubes/application/views/templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title><?= $title; ?></title>
+  <link rel="stylesheet" type="text/css" href="asset/css/main.css">
+</head>
+<body>
+	<header> 
+  </header>
+    <div id="conten">
+      <div id="head">
+        <a target="_blank" href="asset/icon/itera.jpg">
+          <img src="asset/icon/itera.jpg">
+        </a>
+      <h1>SIMPLE LAPOR!</h1>
+  </div>
+  
