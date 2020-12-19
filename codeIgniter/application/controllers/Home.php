76d772a39838030197a8c92820955b0018a80304
Siraz Tri Denira <56485573+deniraz@users.noreply.github.com>
2020-12-19 15:22:37 +0700

    change validation rules

--- codeIgniter/application/controllers/Home.php
+++ codeIgniter/application/controllers/Home.php
@@ -5,6 +5,7 @@ class Home extends CI_Controller
 {
 	public function __construct(){
 		parent::__construct();
+		$this->load->helper(array('form', 'url'));
 		$this->load->model('Lapor_model'); //ngambil model model_lapor dari folder models
 		$this->load->library('form_validation');
 	}
@@ -13,13 +14,14 @@ class Home extends CI_Controller
 		$data['lapor'] = $this->Lapor_model->getlapor(); //getlapor() ada di model Model_lapor
 		$this->load->view('home/index',$data);
 	}
-
+	
 	public function tambah(){
 		
 		//validasi form
-		$this->form_validation->set_rules('isi', 'Isi', 'required|min_length[250]');
+		$this->form_validation->set_rules('isi', 'Laporan', 'required|min_length[100]');
+		
 		if($this->form_validation->run() == FALSE){
-			$this->load->view('tambah/index');
+			$this->load->view('tambah/index');			
 		}else{
 			$this->Lapor_model->tambah();
 			echo "
