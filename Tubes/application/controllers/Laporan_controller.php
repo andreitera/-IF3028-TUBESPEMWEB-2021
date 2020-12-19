5f65993032a749e748e6de160eae6304f9eff892
andika-118140058 <andikas926@gmail.com>
2020-12-19 11:28:59 +0700

    Edit Controller

--- Tubes/application/controllers/Laporan_controller.php
+++ Tubes/application/controllers/Laporan_controller.php
@@ -2,37 +2,51 @@
     defined("BASEPATH") OR exit("No direct script access allowed");
 
     class Laporan_controller extends CI_Controller{
+        
         public function __construct(){
             parent::__construct();
             $this->load->model("laporan_model");
             $this->load->library('form_validation');
-	    }
+        }
+        
         public function index(){
             $data["laporan"] = $this->laporan_model->getAll();
             $this->load->view("tampilan_view", $data); 
         }
 
-        public function add(){
-            $laporan = $this->Laporan_model;
+        public function tambah(){
+            $laporan = $this->laporan_model;
             $validation = $this->form_validation;
             $validation->set_rules($laporan->rules());
 
-            if($validation->run()){
+            if ($validation->run()) {
                 $laporan->save();
-                $this->session->set_flashdata('success','Berhasil dikirim');
+                $this->session->set_flashdata('success', 'Berhasil ditambah');
             }
-        }
 
-        public function tambah(){
             $this->load->view("tambah_view");
         }
-<<<<<<< HEAD
-      
-=======
+
+        public function edit($id = null){
+            if (!isset($id)) redirect('tampilan_view');
+        
+            $laporan = $this->laporan_model;
+            $validation = $this->form_validation;
+            $validation->set_rules($laporan->rules());
+
+            if ($validation->run()) {
+                $laporan->update();
+                $this->session->set_flashdata('success', 'Berhasil diubah');
+            }
+
+            $data["laporan"] = $laporan->getById($id);
+            if (!$data["laporan"]) show_404();
+            
+            $this->load->view("", $data);
+        }
 
         public function detail(){
             $this->load->view("detail_view");
         }
->>>>>>> 5ccc1a962fdba8e9ea7470feed2b00b96da99c85
     }
 ?>
