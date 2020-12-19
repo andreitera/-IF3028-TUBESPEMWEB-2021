c1d0c566879e1f5a68d714cbd35b895a187596b7
Muhammad Najie <57042601+muhammadnajie@users.noreply.github.com>
2020-12-19 14:25:18 +0700

    detail gambar

--- app/Views/detail.php
+++ app/Views/detail.php
@@ -4,6 +4,27 @@
         <article id="detail">
             <p class="pt-2"><?= $item['isi']; ?></p>
             <p class="pt-2">Lampiran: <?= $item['lampiran']; ?></p>
+            <?php
+                $lampiran = $item['lampiran'];
+                $dot = strrpos($lampiran, '.');
+                $file_ext = substr($lampiran, $dot+1);
+
+                $allowed_extension = ['jpeg', 'jpg', 'png', 'svg'];
+                $is_pict = false;
+                foreach($allowed_extension as $ext) {
+                    if($ext === $file_ext) {
+                        $is_pict = true;
+                        break;
+                    }
+                }
+
+                if($is_pict) {
+            ?>
+
+                    <img src="/assets/uploaded_file/<?= $lampiran ?>" width="300" height="300" alt="...">';
+            <?php
+                }
+            ?>
             <!-- <img src="bg_bggenerator_com.png" width="100" height="100" alt=""> -->
             <div class="d-flex mt-2">
                 <p class="flex-grow-1">Waktu: <?= $item['created_at']; ?></p>
