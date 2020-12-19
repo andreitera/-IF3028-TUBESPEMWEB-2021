4abc3c0dd513ccb7c9a1296a236a98b63d727267
Muhammad Lukman Al Hakim <56007339+LukmanAH@users.noreply.github.com>
2020-12-19 18:57:34 +0700

    Add update model

---
+++ application/models/Update_model.php
@@ -0,0 +1,42 @@
+<?php
+class Update_model extends CI_model{
+    public function getUbahLaporan(){
+        $id = $_GET["ubah_id"];
+        $query = $this->db->get_where('tb_laporan', array('id_lapor' => $id));
+        return $query->result_array();
+        
+    }
+    public function updateLaporan(){
+        $id = $_GET["ubah_id"];
+
+        $ekstensi_diperbolehkan	= array('doc','docx','xls','xlsx','ppt','pptx','pdf');
+        $file = $_FILES['myFile']['name'];
+        $x = explode('.', $file);
+        $ekstensi = strtolower(end($x));
+        $ukuran	= $_FILES['myFile']['size'];
+        $file_tmp = $_FILES['myFile']['tmp_name'];
+        
+        move_uploaded_file($file_tmp, 'assets/file/'.$file);
+
+      
+
+    date_default_timezone_set("Asia/Jakarta");
+    // $tgl = date("Y/m/d H:i:s");
+    $now = date("Y-m-d H:i:s");
+    
+    $data = [ 
+        "laporan" => $this->input->post('laporan',true),
+        "lampiran" => $file,
+        "waktu" => $now,
+        "aspek" => $this->input->post('aspek',true)
+    ];
+
+
+    $this->db->update('tb_laporan', $data, array('id_lapor' => $id));
+    echo "<script>alert('Berhasil di ubah');</script>";
+    echo "<script>location='home';</script>";
+    }
+   
+}
+
+?>
