6cd338b15f8572cddc003335e04fecf9b27c674f
Fathan Rizki H <fathanrizki001@gmail.com>
2020-12-19 21:45:24 +0700

    Perbaikan view ajax cari

--- lapor/application/views/cari.php
+++ lapor/application/views/cari.php
@@ -1,5 +1,4 @@
-<div class="line"></div>
-		<?php foreach ($laporan as $value) {?>
+<?php foreach ($laporan as $value) {?>
 				<div class="data">
 				<?php if(strlen($value["komentar"]) > 300){ ?>
 					<p style="margin-bottom: 20px;"><?php echo substr($value["komentar"], 0, 300); ?>.....</p>
@@ -8,9 +7,9 @@
 				<?php } ?>
 				<p> Lampiran : <?= $value["lampiran"];?></p>
 				<label>Waktu : <?= $value["waktu"];?></label>
-				<label>Aspek : <?= $value["aspek"];?></label>
-				<a href="<?= base_url() ?>/home/detail/<?php echo $value["id"] ?>"><p>Selengkapnya ></p></a>
-				<div class="line"></div>
+				<label style="margin-left: 20px;">Aspek : <?= $value["aspek"];?></label>
+				<a class="button-detail" href="<?= base_url() ?>/home/detail/<?php echo $value["id"] ?>"><p>Selengkapnya-></p></a>
+				<div class="line" style="clear:both;"></div>
 			</div>	
 		<?php }?>
 	</div>
