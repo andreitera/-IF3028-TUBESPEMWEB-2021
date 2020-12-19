12a8861456cee13b57b7bdb95dfc4883d4017d7a
AbdillahIlman04 <abdillahilman04@gmail.com>
2020-12-19 10:09:32 +0700

    Update View

---
+++ application/views/tampil_detai.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tampilan Laporan</title>
+
+
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/style/style_detail.css') ?>">
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="content">
+
+            <div class="header">
+
+                <strong>Layanan Aspirasi dan Pengaduan Online Mahasiswa IF ITERA</strong>
+
+                <br><br>
+
+            </div>
+
+
+            <br>
+
+            <p>Detail Laporan/Komentar</p>
+            <hr>
+
+            <br>
+
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <p>Lampiran: </p>
+
+
+            <div class="file">
+
+            </div>
+
+
+
+            <p>Waktu: Aspek: Infrastruktur </p>
+
+            <div class="detail">Hapus Laporan/Komentar</div>
+
+            <hr>
+
+
+
+        </div>
+
+        <br><br>
+
+    </div>
+
+</body>
+
+</html>
