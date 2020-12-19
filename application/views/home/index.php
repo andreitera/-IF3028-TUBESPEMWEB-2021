abe87ed240569e31f39afdeef71cfdfa9aeff9ca
achmadferdi <57844476+achmadferdi@users.noreply.github.com>
2020-12-19 23:39:02 +0700

    Update index.php

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,3 +1,26 @@
-
-    <h1>Hello, world!</h1>
-
+<html>
+    <head>
+        <title>Beranda</title>
+        <link rel="stylesheet" href="<?php echo base_url(''); ?>">
+    </head>
+    <h1 align="center">SIMPLE LAPOR!</h1><br>
+    <br>
+    <body>
+        <form method="GET" action="<?php echo base_url().'cari' ?>">
+            <input type="search" name="cari">
+            <button type="submit" id="tcari">Cari</button>
+        </form>
+        <br>
+        <form method="POST">
+            <table>
+            <tr>
+                <td><p align="center">Buat Laporan/Komentar</p></td>
+                <td><button type="submit" id="tombolbuat">+</button></td>
+            </tr>
+            <tr>
+                <td>Laporan/Komentar Terakhir</td></td>
+            </tr>
+            </table>
+        </form>
+    </body>
+</html>
