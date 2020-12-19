5e2006c4de24852381ad07e92b089c412cee016b
118140102 - M. Galih Pratama <65152944+MGalihPratama@users.noreply.github.com>
2020-12-19 22:46:27 +0700

    Fix tamplate Header dan footer

---
+++ codeT/application/views/templates/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+  <br>
+  <h3>&copy;  Pejuang_Tubes </h3S>
+    <h5> M Galih Pratama | NIM. 118140102 </h5S>
+      <h5> Putri Ayuni S | NIM. 118140118 </h5S>
+        <h5> Manarul Hidayat | NIM. 118140132 </h5S>
+          <hr />
+          </body>
+          </html>
