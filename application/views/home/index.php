72366470140037af33b91f3a08b993d2aca1a614
dhannypramana <p.dhannypramana@gmail.com>
2020-12-20 22:45:06 +0700

    replace index

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,26 +1,60 @@
-<html>
-    <head>
-        <title>Beranda</title>
-        <link rel="stylesheet" href="<?php echo base_url(''); ?>">
-    </head>
-    <h1 align="center">SIMPLE LAPOR!</h1><br>
-    <br>
-    <body>
-        <form method="GET" action="<?php echo base_url().'cari' ?>">
-            <input type="search" name="cari">
-            <button type="submit" id="tcari">Cari</button>
-        </form>
-        <br>
-        <form method="POST">
-            <table>
-            <tr>
-                <td><p align="center">Buat Laporan/Komentar</p></td>
-                <td><button type="submit" id="tombolbuat">+</button></td>
-            </tr>
-            <tr>
-                <td>Laporan/Komentar Terakhir</td></td>
-            </tr>
-            </table>
-        </form>
-    </body>
-</html>
+<fieldset>
+	<div class="judul">
+		<br><br>
+		<div class="wrapper">
+			<h1> SIMPLE LAPOR! </h1>
+		</div>
+	</div>
+	<br>
+	</div>
+
+	<form name="formHome" action="" method="post">
+		<div class="search">
+			<br><br>
+			<input type="text" name="keyword" id="keyword" placeholder="Telusuri Laporan/Komentar">
+			<button type="submit" value="Cari" id="tombol-cari">Search</button>
+	</form>
+	</div>
+
+	<?php if ($this->session->flashdata('flash')) : ?>
+		<div>Data <strong>berhasil</strong> ditambahkan!<?= $this->session->flashdata('flash'); ?>
+		</div>
+	<?php endif; ?>
+
+	<div class="buat" id="buat">
+		<a href="<?= base_url('auth') ?>"> Input Laporan/Komentar + </a>
+	</div>
+	<br><br>
+	<hr>
+	<div class="tulisan">
+		<p>Laporan/Komentar</p>
+		<hr />
+	</div>
+	<div id="container">
+		<div class="tulisan2">
+			<div class="newshead">
+				<ul class="detail">
+					<hr><br>
+					<?php foreach ($lapor as $dt) : ?>
+						<?php $limitedKata = $this->Data_model->limitKata($dt['lapor'], 5);?>
+						<?= $limitedKata . ' '; ?>
+						<br>
+						<?= $dt['aspek'] . ' '; ?> <br>
+						<table>
+							<tr>
+								<td><?= $dt['gambar'] . ' '; ?></td>
+								<td id="waktu">
+									<p>
+										<small style="margin-right: -40px;"><?= $dt['waktu'] . ' '; ?></small>
+										<a href="<?= base_url() ?>data/detail/<?= $dt['id']; ?>" style="text-decoration:none"><small>| Lihat Selengkapnya > </small></a></p>
+								</td>
+							</tr>
+						</table>
+						<hr />
+					<?php endforeach ?>
+				</ul>
+			</div>
+		</div>
+	</div>
+</fieldset>
+<script src="<?= base_url(); ?>asset/js/ajax.js"></script>
