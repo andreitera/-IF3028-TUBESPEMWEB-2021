a74b5cab3c97340549d2055af3b2a19fb280bd99
Ester Efritira <60662711+estfritira@users.noreply.github.com>
2021-12-25 20:49:51 +0700

    Update Search.php

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -18,7 +18,7 @@ class Search extends CI_Controller {
 		else{
 			$data['keyword']= null;
 		}
-		$data ['judul'] = "Tugas Besar PWB 118140048-118140103-118140105";
+		$data ['judul'] = "Tugas Besar PWB 118140086";
 		$data ['cari'] = $this->Search_model->cariData($data['keyword']);
 		// $this->load->database();
 		$this->load->view('Search/index',$data);
